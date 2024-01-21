fe91c679684aa542c2f90127920a93c0c8b057cd
hyoni0817 <hyoni0817@naver.com>
2024-01-21 22:13:58 +0900

    Set: svg를 로드할 수 있도록 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 const stylexPlugin = require('@stylexjs/nextjs-plugin');
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = stylexPlugin({
