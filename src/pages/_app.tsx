a72fd82ce251033a6d9026963897b08424fd6f06
hyoni0817 <hyoni0817@naver.com>
2024-02-06 23:32:39 +0900

    Feat: react-slick 스타일 적용을 위해 관련 css 파일 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from 'next/app';
 import '@assets/global.css';
 import '@assets/reset.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import '../features/authentication/assets/onboarding-slider.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
