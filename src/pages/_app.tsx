2f0facc8bad9c40390dc2a9f4a8501e1326fee5f
hyoni0817 <hyoni0817@naver.com>
2024-02-12 19:57:05 +0900

    Feat: _app에 redux 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from '../store';
 import '@assets/global.css';
 import '@assets/reset.css';
 import 'slick-carousel/slick/slick.css';
@@ -6,5 +8,9 @@ import 'slick-carousel/slick/slick-theme.css';
 import '../features/authentication/assets/onboarding-slider.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
