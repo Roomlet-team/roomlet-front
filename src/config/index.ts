030ab05866d2c258b7d78ccc4ec081353af6ebed
hyoni0817 <hyoni0817@naver.com>
2024-02-13 23:15:51 +0900

    Feat: s3 이미지 url을 관리하는 config.js 파일 생성

---
+++ src/config/index.ts
@@ -0,0 +1,1 @@
+export { default as s3ImgUrlConfig } from './s3ImgUrlConfig';
---
+++ src/config/s3ImgUrlConfig.ts
@@ -0,0 +1,6 @@
+// 어떤 컴포넌트에서든 공통으로 사용할 s3 이미지 url 관리
+const s3ImgUrlConfig = {
+  defaultProfile: 'https://roomlet-front.s3.ap-northeast-2.amazonaws.com/public/images/profile/default.png',
+};
+
+export default s3ImgUrlConfig;
