06556c6714f9b0e4ba3273436dd71e7bd2ee693f
hyoni0817 <hyoni0817@naver.com>
2024-02-17 23:58:18 +0900

    Update: GnbNavLayout에 배경색 추가 및 기존 스타일 코드 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,12 @@ import React from 'react';
 import stylex from '@stylexjs/stylex';
 import GnbNavLayout from '@src/layouts/GnbNavLayout';
 
-const styles = stylex.create({
-  base: {
-    fontSize: 16,
-    lineHeight: 1.5,
-    color: 'red',
-  },
-  highlighted: {
-    color: 'rebeccapurple',
-  },
-});
-
 const Home = () => {
   console.log('');
 
   return (
-    <GnbNavLayout>
-      <div {...stylex.props(styles.base)}>Hello, Next.js</div>
+    <GnbNavLayout backgroundColor="#FAFAFA">
+      <div>Hello, Next.js</div>
     </GnbNavLayout>
   );
 };
