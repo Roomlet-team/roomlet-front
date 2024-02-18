6d64d7a236c0e7ca45f02303fa8b0827ad608c15
hyoni0817 <hyoni0817@naver.com>
2024-02-19 00:01:51 +0900

    Feat: 홈 화면에 회의 현황 영역 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import stylex from '@stylexjs/stylex';
 import GnbNavLayout from '@src/layouts/GnbNavLayout';
 import UserGreeting from '@src/features/home/components/UserGreeting';
+import MeetingStatus from '@src/features/home/components/MeetingStatus';
 
 const Home = () => {
   console.log('');
@@ -10,9 +12,28 @@ const Home = () => {
       {/* 프로필 섹션 */}
       <UserGreeting />
       {/* 회의 현황 섹션 */}
+      <section {...stylex.props(Styles.meetingStatusSection)}>
+        <h2 {...stylex.props(Styles.sectionTitle)}>회의 현황</h2>
+        <MeetingStatus />
+      </section>
       {/* 타임 라인 섹션 */}
     </GnbNavLayout>
   );
 };
 
 export default Home;
+
+const Styles = stylex.create({
+  meetingStatusSection: {
+    width: '100%',
+    padding: '0 16px',
+    marginBottom: '24px',
+  },
+  sectionTitle: {
+    marginBottom: '16px',
+    fontSize: '1.6rem',
+    fontWeight: '600',
+    lineHeight: '2.4rem',
+    color: '#616161',
+  },
+});
