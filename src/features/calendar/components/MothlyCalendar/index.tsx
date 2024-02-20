9c385e8134f6e8594046108952a442c8aa9b430e
hyoni0817 <hyoni0817@naver.com>
2024-02-21 01:43:16 +0900

    Feat: 디자인이 적용되어 있지 않은 월간 캘린더 로직 구현

--- src/features/calendar/components/MothlyCalendar/index.tsx
+++ src/features/calendar/components/MothlyCalendar/index.tsx
@@ -3,9 +3,57 @@ import React from 'react';
 import stylex from '@stylexjs/stylex';
 
 const MonthlyCalendar = () => {
+  const dayOfTheWeek = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+  const currentMonth = dayjs().month();
+  const startDay = Number(dayjs().startOf('month').month(currentMonth).day()); // 1일에 해당하는 요일
+  //   const endDay = Number(dayjs().endOf('month').month(currentMonth).day()); // 마지막날에 해당하는 요일
+  const lastDateOfTheMonth = Number(dayjs().month(currentMonth).endOf('month').format('DD')); // 이번달의 마지막 날
+  const numOfWeek = Math.floor(lastDateOfTheMonth / 7) + (startDay > 4 ? 2 : 1); // 1일이 금요일부터 시작되면 한 주를 더 해준다.
+  // 지난달
+  const lastDateOfTheLastMonth = Number(
+    dayjs()
+      .endOf('month')
+      .month(currentMonth - 1 === -1 ? 11 : currentMonth - 1)
+      .format('DD')
+  ); // 지난달의 마지막 날
+
   return (
     <div>
       <h1 {...stylex.props(Styles.currentMonthText)}>{dayjs().format('YYYY.MM')}</h1>
+      <table>
+        <thead>
+          <tr>{React.Children.toArray(dayOfTheWeek.map((day) => <th>{day}</th>))}</tr>
+        </thead>
+        <tbody>
+          {React.Children.toArray(
+            Array(numOfWeek)
+              .fill('')
+              .map((_, weekIdx) => (
+                <tr>
+                  {React.Children.toArray(
+                    Array(7)
+                      .fill('')
+                      .map((_, dateIdx) => {
+                        const dateValue = weekIdx * 7 - startDay + (dateIdx + 1);
+                        // 이번달
+                        if (dateValue > 0 && dateValue <= lastDateOfTheMonth) {
+                          return <td>{dateValue}</td>;
+                        }
+                        // 지난달
+                        if (dateValue <= 0) {
+                          return <td>{dateValue + lastDateOfTheLastMonth + 2}</td>;
+                        }
+                        // 다음달
+                        if (dateValue > lastDateOfTheMonth) {
+                          return <td>{dateValue - lastDateOfTheMonth}</td>;
+                        }
+                      })
+                  )}
+                </tr>
+              ))
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
