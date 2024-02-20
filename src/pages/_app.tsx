ec9f9982b802019d3e862f3f8da84ad398380d38
hyoni0817 <hyoni0817@naver.com>
2024-02-21 00:23:52 +0900

    Feat: 어플리케이션 전역에 days.js로 서울 타임존 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from 'next/app';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+// 타임존 설정
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+// redux
 import { Provider } from 'react-redux';
 import { store } from '../store';
+// etc
 import '@assets/global.css';
 import '@assets/reset.css';
 import 'slick-carousel/slick/slick.css';
@@ -8,6 +15,15 @@ import 'slick-carousel/slick/slick-theme.css';
 import '../features/authentication/assets/onboarding-slider.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+
+  // 기본 타임존 설정
+  dayjs.tz.setDefault('Asia/Seoul');
+
+  // 로케일 설정
+  dayjs.locale('ko');
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
