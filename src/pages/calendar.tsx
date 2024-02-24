e1d501349704a5b211585fcdda4eb7ff7a5d3214
hyoni0817 <hyoni0817@naver.com>
2024-02-25 00:02:59 +0900

    Rename: MonthlyCalendar 컴포넌트명 오타 수정

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import GnbNavLayout from '@src/layouts/GnbNavLayout';
-import MonthlyCalendar from '@src/features/calendar/components/MothlyCalendar';
+import MonthlyCalendar from '@src/features/calendar/components/MonthlyCalendar';
 
 const Calendar = () => {
   return (
