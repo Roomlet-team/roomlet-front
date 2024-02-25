975dc44f6303c423f8cdca84ebe8e3b3b3678929
hyoni0817 <hyoni0817@naver.com>
2024-02-26 00:51:40 +0900

    Feat: 캘린더 페이지에서 선택한 날짜에 대한 회의 리스트를 보는 영역 추가

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import GnbNavLayout from '@src/layouts/GnbNavLayout';
 import MonthlyCalendar from '@src/features/calendar/components/MonthlyCalendar';
+import MeetingSchedule from '@src/features/calendar/components/MeetingSchedule';
 
 const Calendar = () => {
   return (
     <GnbNavLayout>
+      {/* 월간 캘린더 */}
       <MonthlyCalendar />
+      {/* 캘린더에서 선택한 날짜에 대한 회의 리스트 보는 영역 */}
+      <MeetingSchedule />
     </GnbNavLayout>
   );
 };
