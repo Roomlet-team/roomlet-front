444b16cb414b6f41ccd3a57f42d6867df8833124
hyoni0817 <hyoni0817@naver.com>
2024-04-20 23:56:26 +0900

    Update: import한 Typography를 상대 경로로 변경

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import Link from 'next/link';
 import stylex from '@stylexjs/stylex';
-import { Typography } from '@public/styles/vars.stylex';
+import { Typography } from '../../../../public/styles/vars.stylex';
 import ArrowHeadOutlined from '@src/components/icons/ArrowHeadOutlined';
 
 type HeaderProps = {
