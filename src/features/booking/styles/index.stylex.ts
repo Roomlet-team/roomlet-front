b50adfb0a6f25b693b9ce7ca71b5e58354ebb5d5
hyoni0817 <hyoni0817@naver.com>
2024-04-21 23:43:09 +0900

    Feat: input 형식의 booking용 form Item에 아이콘과 함께 나타내는 스타일을 추가

--- src/features/booking/styles/index.stylex.ts
+++ src/features/booking/styles/index.stylex.ts
@@ -1,9 +1,14 @@
-import { colors } from '@public/styles/vars.stylex';
+import { colors } from '../../../../public/styles/vars.stylex';
 import stylex from '@stylexjs/stylex';
 
 export const BookingStyles = stylex.create({
   formItemInputWrapper: {
     padding: '14px 12px',
+    boxShadow: `inset 0 0 0 1px ${colors.gray40}`,
+    borderRadius: '12px',
+  },
+  formItemInputWithIconWrapper: {
+    padding: '12px',
     boxShadow: `inset 0 0 0 1px ${colors.gray40}`,
     borderRadius: '12px',
   },
