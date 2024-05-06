1d45263c533f563a01a5a3899566b1dc57075350
hyoni0817 <hyoni0817@naver.com>
2024-05-06 20:29:40 +0900

    Feat: Outline 형태의 화살표 머리 모양 아이콘 컴포넌트 구현

---
+++ src/components/icons/ArrowHeadOutlinedV2.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react';
+import stylex from '@stylexjs/stylex';
+import { IconProps } from './types';
+
+const ArrowHeadOutlinedV2: FC<IconProps> = (props) => {
+  const { width, height, rotate } = props;
+
+  return (
+    <svg
+      {...stylex.props(Styles.Wrapper(rotate))}
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      fill="none"
+      {...props}
+    >
+      <g clipPath="url(#a)">
+        <path fill="#333" d="M8.59 16.59 13.17 12 8.59 7.41 10 6l6 6-6 6z" />
+      </g>
+      <defs>
+        <clipPath id="a">
+          <path fill="#fff" d="M0 0h24v24H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+export default ArrowHeadOutlinedV2;
+
+const Styles = stylex.create({
+  Wrapper: (rotate) => ({
+    transform: `rotate(${rotate || 0}deg)`,
+  }),
+});
