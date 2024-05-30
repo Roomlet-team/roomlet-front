ba5911ba4978c6143f3f7411b9f2faecb7e2d87f
hyoni0817 <hyoni0817@naver.com>
2024-05-30 19:11:17 +0900

    Feat: 스크롤 여부를 선택할지 말지 결정하는 prop 추가

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,23 +2,24 @@ import React, { FC } from 'react';
 import stylex from '@stylexjs/stylex';
 
 type MainLayoutProps = {
+  isScroll: boolean; // 전체 화면에 스크롤이 적용되게 할지 말지 결정
   children: React.ReactElement | React.ReactElement[] | string;
 };
 
 const MainLayout: FC<MainLayoutProps> = (props) => {
-  const { children } = props;
+  const { isScroll, children } = props;
 
-  return <div {...stylex.props(Styles.container)}>{children}</div>;
+  return <div {...stylex.props(Styles.container(isScroll))}>{children}</div>;
 };
 
 export default MainLayout;
 
 const Styles = stylex.create({
-  container: {
+  container: (isScroll) => ({
     width: '100%',
     height: '100%',
     margin: '0 auto',
     maxWidth: '767px',
-    overflowY: 'hidden',
-  },
+    overflowY: isScroll ? 'auto' : 'hidden',
+  }),
 });
