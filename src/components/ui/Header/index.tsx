be06cc2d44573d8aa0151e2fc964fdca9c2f6027
hyoni0817 <hyoni0817@naver.com>
2024-06-20 00:01:01 +0900

    Feat: Headers에 이전으로 이동할 prevUrl 추가

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -5,17 +5,20 @@ import { Typography } from '../../../../public/styles/vars.stylex';
 import ArrowHeadOutlined from '@src/components/icons/ArrowHeadOutlined';
 
 type HeaderProps = {
-  title: string;
+  title: string; // 제목
+  prevUrl?: string; // 이전으로 이동할 url
 };
 
 const Header: FC<HeaderProps> = (props) => {
-  const { title } = props;
+  const { title, prevUrl } = props;
 
   return (
     <div {...stylex.props(Styles.container)}>
-      <Link href="/calendar">
-        <ArrowHeadOutlined width={24} height={24} />
-      </Link>
+      {prevUrl && (
+        <Link href={prevUrl}>
+          <ArrowHeadOutlined width={24} height={24} />
+        </Link>
+      )}
       <h1 {...stylex.props(Typography.TitleSmallSemiBold)}>{title}</h1>
     </div>
   );
