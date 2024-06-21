f35c3bcb985cac2716c76fb07848db232591fa9e
hyoni0817 <hyoni0817@naver.com>
2024-06-21 23:55:03 +0900

    Feat: MypageMenu를 마이페이지에서 공통으로 사용할 수 있도록 수정 및 MenuList 속성 타입스크립트 구현

--- src/features/mypage/compontents/MypageMenu/index.tsx
+++ src/features/mypage/compontents/MypageMenu/index.tsx
@@ -1,30 +1,24 @@
-import React from 'react';
+import React, { FC } from 'react';
 import stylex from '@stylexjs/stylex';
 import Link from 'next/link';
-import SettingOutlined from '@src/components/icons/SettingOutlined';
-import AddUserOutlined from '@src/components/icons/AddUserOutlined';
-import MegaphoneOutlined from '@src/components/icons/MegaphoneOutlined';
-import BellOutlined from '@src/components/icons/BellOutlined';
-import ExitOutlined from '@src/components/icons/ExitOutlined';
 import { Typography, colors } from '../../../../../public/styles/vars.stylex';
+import { MenuListType } from './index.type';
 
-const MypageMenu = () => {
-  const menuList = [
-    { id: 1, name: '워크스페이스 설정', icon: <SettingOutlined width={24} height={24} />, href: '' },
-    { id: 2, name: '멤버초대', icon: <AddUserOutlined width={24} height={24} />, href: '' },
-    { id: 3, name: '공지사항 작성', icon: <MegaphoneOutlined width={24} height={24} />, href: '' },
-    { id: 4, name: '알림설정', icon: <BellOutlined width={24} height={24} />, href: '' },
-    { id: 5, name: '워크스페이스 나가기', icon: <ExitOutlined width={24} height={24} />, href: '' },
-  ];
+type MypageMenu = {
+  menuList: MenuListType;
+};
+
+const MypageMenu: FC<MypageMenu> = (props) => {
+  const { menuList } = props;
 
   return (
     <nav>
       <ul {...stylex.props(Styles.List)}>
         {menuList.map((item) => (
-          <li key={item.id}>
-            <Link href={item.href} {...stylex.props(Styles.MenuLink, Typography.TextSmallMedium)}>
+          <li key={item.id} {...stylex.props(Styles.Item)}>
+            <Link href={item.href} {...stylex.props(Styles.MenuLink)}>
               <span>{item.icon}</span>
-              <span>{item.name}</span>
+              <span {...stylex.props(Typography.TextSmallMedium)}>{item.name}</span>
             </Link>
           </li>
         ))}
@@ -40,8 +34,10 @@ const Styles = stylex.create({
     listStyle: 'none',
     flexDirection: 'column',
   },
-  MenuLink: {
+  Item: {
     padding: '16px',
+  },
+  MenuLink: {
     display: 'flex',
     gap: '16px',
     alignItems: 'center',
