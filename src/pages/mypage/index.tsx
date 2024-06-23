b11d38ad7020ddbcc3661a4dac9adf8ea4996bd1
hyoni0817 <hyoni0817@naver.com>
2024-06-23 23:15:42 +0900

    Feat: 마이페이지 홈에서 마이페이지 메뉴 컴포넌트에 마이페이지 메뉴 리스트 prop 전달되는 방식으로 수정

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -5,8 +5,27 @@ import stylex from '@stylexjs/stylex';
 import { colors } from '../.../../../../public/styles/vars.stylex';
 import MypageSummaryProfile from '@src/features/mypage/compontents/MypageSummaryProfile';
 import MypageMenu from '@src/features/mypage/compontents/MypageMenu';
+import SettingOutlined from '@src/components/icons/SettingOutlined';
+import AddUserOutlined from '@src/components/icons/AddUserOutlined';
+import MegaphoneOutlined from '@src/components/icons/MegaphoneOutlined';
+import BellOutlined from '@src/components/icons/BellOutlined';
+import ExitOutlined from '@src/components/icons/ExitOutlined';
 
 const MyPageHome = () => {
+  const commonUrl = 'mypage';
+  const menuList = [
+    {
+      id: 1,
+      name: '워크스페이스 설정',
+      icon: <SettingOutlined width={24} height={24} />,
+      href: `/${commonUrl}/workspace`,
+    },
+    { id: 2, name: '멤버초대', icon: <AddUserOutlined width={24} height={24} />, href: '' },
+    { id: 3, name: '공지사항 작성', icon: <MegaphoneOutlined width={24} height={24} />, href: '' },
+    { id: 4, name: '알림설정', icon: <BellOutlined width={24} height={24} />, href: '' },
+    { id: 5, name: '워크스페이스 나가기', icon: <ExitOutlined width={24} height={24} />, href: '' },
+  ];
+
   return (
     <GnbNavLayout backgroundColor={colors.white500}>
       <Header title="마이페이지" />
@@ -15,7 +34,7 @@ const MyPageHome = () => {
       {/* 경계선 */}
       <div {...stylex.props(Styles.BoundaryArea)} />
       {/* 마이페이지 메뉴 */}
-      <MypageMenu />
+      <MypageMenu menuList={menuList} />
     </GnbNavLayout>
   );
 };
