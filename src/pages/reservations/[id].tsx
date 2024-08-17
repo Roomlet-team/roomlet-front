345bea11cf694f76a10d1d635596a153aca6ab2d
hyoni0817 <hyoni0817@naver.com>
2024-08-17 23:57:04 +0900

    Feat: 퍼블리싱을 제외하고 대략적인 요소 위치만 잡은 회의 정보 페이지 구현

---
+++ src/pages/reservations/[id].tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const CongressInfo = () => {
+  return (
+    <div>
+      {/* 회의 정보 */}
+      <div>
+        <div>디자인</div>
+        <div>
+          <h1>사용성 관련 기획 회의</h1>
+          <button type="button">더보기</button>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <div>장소</div>
+              <div>C 회의실</div>
+            </li>
+            <li>
+              <div>날짜/시간</div>
+              <div>2024-03-06 10:00 ~ 11:00</div>
+            </li>
+            <li>
+              <div>참여자</div>
+              <div>
+                <div>기획팀</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div>상세내용</div>
+          <div>홍길동 인턴: 당뇨 관리앱 분석 조사 발표</div>
+        </div>
+      </div>
+
+      {/* 댓글 */}
+      <div>댓글</div>
+    </div>
+  );
+};
+
+export default CongressInfo;
