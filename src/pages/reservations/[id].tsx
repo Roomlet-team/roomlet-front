d3b30e584ed1f7693ff45ac0eacd4fd63f8da05a
hyoni0817 <hyoni0817@naver.com>
2024-09-10 23:33:42 +0900

    Feat: 예약 상세보기에서 카테고리 태그 및 회의 제목 영역 퍼블리싱

--- src/pages/reservations/[id].tsx
+++ src/pages/reservations/[id].tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import stylex from '@stylexjs/stylex';
-import { Typography } from '../../../public/styles/vars.stylex';
+import { colors, Typography } from '../../../public/styles/vars.stylex';
 import MainLayout from '@src/layouts/MainLayout';
+import EllipsisOutlined from '@src/components/icons/EllipsisOutlined';
 
 const MeetingDetails = () => {
   return (
     <MainLayout>
       {/* 회의 정보 */}
       <div>
-        <div>디자인</div>
-        <div>
-          <h1 {...stylex.props(Typography.TextLargeBold)}>사용성 관련 기획 회의</h1>
-          <button type="button">더보기</button>
+        {/* 카테고리 태그 및 회의 제목 */}
+        <div {...stylex.props(Styles.categoryAndTitleContainer)}>
+          <div {...stylex.props(Styles.categoryTag)}>디자인</div>
+          <div {...stylex.props(Styles.TitleAndShowMoreContainer)}>
+            <h1 {...stylex.props(Typography.TextLargeBold)}>사용성 관련 기획 회의</h1>
+            <button type="button">
+              <EllipsisOutlined width="24" heigh="24" />
+            </button>
+          </div>
         </div>
+
+        {/* 회의실 정보 */}
         <div>
           <ul>
             <li>
@@ -44,3 +52,23 @@ const MeetingDetails = () => {
 };
 
 export default MeetingDetails;
+
+const Styles = stylex.create({
+  categoryAndTitleContainer: {
+    padding: '16px',
+  },
+  categoryTag: {
+    width: 'fit-content',
+    padding: '4px 8px',
+    marginBottom: '12px',
+    borderRadius: '20px',
+    fontSize: '1.2rem',
+    fontWeight: 500,
+    color: colors.white500,
+    background: colors.red300,
+  },
+  TitleAndShowMoreContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+});
