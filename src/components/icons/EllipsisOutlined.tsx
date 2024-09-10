7df0659762645a870befc1e2efc4a7922e248a50
hyoni0817 <hyoni0817@naver.com>
2024-09-10 23:32:36 +0900

    Feat: Outline 형태의 ellipsis 모양 아이콘 컴포넌트 구현

---
+++ src/components/icons/EllipsisOutlined.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+const EllipsisOutlined = (props) => {
+  const { width, height } = props;
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} fill="none" {...props}>
+      <g clipPath="url(#a)">
+        <path
+          fill="#999"
+          d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2m0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2m0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2"
+        />
+      </g>
+      <defs>
+        <clipPath id="a">
+          <path fill="#fff" d="M0 0h24v24H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+export default EllipsisOutlined;
