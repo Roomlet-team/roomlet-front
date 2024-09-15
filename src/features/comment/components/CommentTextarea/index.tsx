71c06d5f0d1e83d2d97276a5c8d8439dabdd2d8f
hyoni0817 <hyoni0817@naver.com>
2024-09-16 01:55:33 +0900

    Feat: 댓글 작성란의 높이가 최대 3줄까지 자동으로 조절되게 구현

--- src/features/comment/components/CommentTextarea/index.tsx
+++ src/features/comment/components/CommentTextarea/index.tsx
@@ -1,23 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import stylex from '@stylexjs/stylex';
 import useTextArea from '@src/hooks/useTextArea';
 import { colors } from '../../../../../public/styles/vars.stylex';
 import SendFilled from '@src/components/icons/SendFilled';
+import { title } from 'process';
 
 const CommentTextarea = () => {
   const [value, handleChangeValue] = useTextArea('');
+
+  useEffect(() => {
+    // 컨텐츠 양에 맞게 textarea가 자동으로 조절되게 하는 코드
+    const commentTextarea = document.getElementById('comment');
+    commentTextarea.style.height = '20px'; // 높이 리셋
+    commentTextarea.style.height = `${commentTextarea.scrollHeight}px`; // 스크롤이 생기게 될 높이 설정
+  }, [value]);
 
   return (
     <div {...stylex.props(Styles.container)}>
       <div {...stylex.props(Styles.textareaAndSubmitContainer)}>
         <textarea
           {...stylex.props(Styles.textarea)}
+          id="comment"
           value={value}
           onChange={handleChangeValue}
           rows={1}
           placeholder="댓글을 입력해주세요."
         />
-        <SendFilled width={32} height={32} />
+        <div {...stylex.props(Styles.iconWrapper)}>
+          <SendFilled width={32} height={32} />
+        </div>
       </div>
     </div>
   );
@@ -33,17 +44,30 @@ const Styles = stylex.create({
   },
   textareaAndSubmitContainer: {
     height: 'auto',
-    padding: '12px',
+    padding: '8px 12px',
     display: 'flex',
+    alignItems: 'center',
     gap: '16px',
     border: `1px solid #d4d4d4`,
     borderRadius: '16px',
   },
   textarea: {
     width: '100%',
-    // height: '20px',
+    maxHeight: '60px',
     padding: 0,
     border: 'none',
     resize: 'none',
+    fontSize: '1.4rem',
+    outline: 'none',
+    overflow: 'auto',
+    color: {
+      default: colors.black500,
+      ':placeholder': colors.gray60,
+    },
+  },
+  iconWrapper: {
+    lineHeight: 1,
+    flexShrink: 0,
+    alignSelf: 'flex-end',
   },
 });
