885e74b45d8bf81ac6f3bafbfec1c0d2f8c65a10
hyoni0817 <hyoni0817@naver.com>
2024-11-02 18:34:03 +0900

    Feat: 함수 호출로 confirm 모달을 불러올 수 있게 구현

--- src/components/ui/Modal/confirm/index.tsx
+++ src/components/ui/Modal/confirm/index.tsx
@@ -3,27 +3,76 @@ import stylex from '@stylexjs/stylex';
 import Modal from '..';
 import { colors, Typography } from '../../../../../public/styles/vars.stylex';
 import { ModalProps } from '../types';
+import { createRoot } from 'react-dom/client';
 
-type ConfirmModalProps = {
+let root = null;
+const confirmModalId = 'confirm-modal';
+
+interface ConfirmModalProps {
   content: string;
   onOk: (e: React.MouseEvent) => void;
   onCancel: (e: React.MouseEvent) => void;
   okBtnName?: string;
   cancelBtnName?: string;
-};
+}
 
 const ConfirmModal: FC<ModalProps & ConfirmModalProps> = (props) => {
   const { content, onOk, onCancel, okBtnName, cancelBtnName } = props;
+
+  /**
+   * 모달 닫기
+   */
+  const close = () => {
+    const targetConfirmModalElement = document.getElementById(confirmModalId);
+
+    if (targetConfirmModalElement) {
+      root.unmount(targetConfirmModalElement);
+    }
+  };
+
+  /**
+   * 취소 버튼을 눌렀을 때
+   */
+  const handleClickCancel = (e) => {
+    // onCancel가 존재하면, onCandle 함수 실행
+    if (onCancel) {
+      onCancel(e);
+    }
+
+    // 모달 닫기
+    close();
+  };
+
+  /**
+   * 확인 버튼을 눌렀을 때
+   */
+  const handleClickOk = (e) => {
+    // onOk가 존재하면, onOk 함수 실행
+    if (onOk) {
+      onOk(e);
+    }
+
+    // 모달 닫기
+    close();
+  };
 
   return (
     <Modal isOpen>
       <div {...stylex.props(Styles.container)}>
         <p {...stylex.props(Typography.SubTextLargeRegular, Styles.content)}>{content}</p>
         <div {...stylex.props(Styles.btnContainer)}>
-          <button type="button" onClick={onCancel} {...stylex.props(Styles.cancelBtn, Typography.SubTextLargeSemiBold)}>
+          <button
+            type="button"
+            onClick={handleClickCancel}
+            {...stylex.props(Styles.cancelBtn, Typography.SubTextLargeSemiBold)}
+          >
             {cancelBtnName || '취소'}
           </button>
-          <button type="button" onClick={onOk} {...stylex.props(Styles.okBtn, Typography.SubTextLargeSemiBold)}>
+          <button
+            type="button"
+            onClick={handleClickOk}
+            {...stylex.props(Styles.okBtn, Typography.SubTextLargeSemiBold)}
+          >
             {okBtnName || '확인'}
           </button>
         </div>
@@ -32,7 +81,17 @@ const ConfirmModal: FC<ModalProps & ConfirmModalProps> = (props) => {
   );
 };
 
-export default ConfirmModal;
+const confirm = (props) => {
+  // body 태그에 div 태그를 추가하여 모달 띄우기
+  const divElement = document.createElement('div');
+  divElement.id = confirmModalId;
+  document.body.appendChild(divElement);
+  root = createRoot(divElement);
+
+  root.render(<ConfirmModal {...props} />);
+};
+
+export { confirm };
 
 const Styles = stylex.create({
   container: {
