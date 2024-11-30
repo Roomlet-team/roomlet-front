704e8a70c1c664816ab622f8dc39c455c92ddeff
hyoni0817 <hyoni0817@naver.com>
2024-12-01 00:52:34 +0900

    Feat: 워크스페이스가 존재하지 않는 경우에 대한 마이페이지 메뉴 리스트 구현

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -11,9 +11,13 @@ import ExitOutlined from '@src/components/icons/ExitOutlined';
 import BoundaryArea from '@src/components/ui/BoundaryArea';
 import { confirm } from '@src/components/ui/Modal/confirm';
 import MyPageInviteModal from '@src/features/mypage/compontents/MyPageInviteModal';
+import useGetWorkspaceListQuery from '@src/queries/workspace/useGetWorkspaceListQuery';
+import AddOutlined from '@src/components/icons/AddOutlined';
+import EntranceOutlined from '@src/components/icons/EntranceOutlined';
 
 const MyPageHome = () => {
   const [isInviteModalOpen, setIsInviteModalOpen] = useState<boolean>(false);
+  const { data } = useGetWorkspaceListQuery();
   const commonUrl = 'mypage';
 
   // 멤버 초대 모달을 열고 닫게하는 함수
@@ -21,36 +25,53 @@ const MyPageHome = () => {
     setIsInviteModalOpen(!isInviteModalOpen);
   };
 
-  const menuList = [
-    {
-      id: 1,
-      name: '워크스페이스 설정',
-      icon: <SettingOutlined width={24} height={24} />,
-      href: `/${commonUrl}/workspace`,
-    },
-    {
-      id: 2,
-      name: '멤버초대',
-      icon: <AddUserOutlined width={24} height={24} />,
-      onClick: () => handleClickInviteModal(),
-    },
-    { id: 4, name: '알림 설정', icon: <BellOutlined width={24} height={24} />, href: `/${commonUrl}/alarm` },
-    {
-      id: 5,
-      name: '워크스페이스 나가기',
-      icon: <ExitOutlined width={24} height={24} />,
-      onClick: () => {
-        confirm({
-          content:
-            '워크스페이스를 나가도 작성된 회의 내용은 남아있어요. 다시 참여를 원하시면 초대 링크를 입력 후 참여 가능합니다.',
-          okBtnName: '나갈래요',
-          onOk: null,
-          cancelBtnName: '유지할래요',
-          onCancel: null,
-        });
-      },
-    },
-  ];
+  const menuList = data?.workspaceCount
+    ? [
+        // 워크 스페이스가 존재하는 경우
+        {
+          id: 1,
+          name: '워크스페이스 설정',
+          icon: <SettingOutlined width={24} height={24} />,
+          href: `/${commonUrl}/workspace`,
+        },
+        {
+          id: 2,
+          name: '멤버초대',
+          icon: <AddUserOutlined width={24} height={24} />,
+          onClick: () => handleClickInviteModal(),
+        },
+        { id: 4, name: '알림 설정', icon: <BellOutlined width={24} height={24} />, href: `/${commonUrl}/alarm` },
+        {
+          id: 5,
+          name: '워크스페이스 나가기',
+          icon: <ExitOutlined width={24} height={24} />,
+          onClick: () => {
+            confirm({
+              content:
+                '워크스페이스를 나가도 작성된 회의 내용은 남아있어요. 다시 참여를 원하시면 초대 링크를 입력 후 참여 가능합니다.',
+              okBtnName: '나갈래요',
+              onOk: null,
+              cancelBtnName: '유지할래요',
+              onCancel: null,
+            });
+          },
+        },
+      ]
+    : [
+        // 워크 스페이스가 존재하지 않는 경우
+        {
+          id: 1,
+          name: '워크스페이스 생성하기',
+          icon: <AddOutlined width={24} height={24} />,
+          href: `/create-workspace`,
+        },
+        {
+          id: 2,
+          name: '워크스페이스 입장하기',
+          icon: <EntranceOutlined width={24} height={24} />,
+          href: `/${commonUrl}/workspace`,
+        },
+      ];
 
   return (
     <>
