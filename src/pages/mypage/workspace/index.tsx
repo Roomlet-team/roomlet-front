b34aea7ffe7118223199e89102465cccfd89d347
hyoni0817 <hyoni0817@naver.com>
2024-12-02 01:31:38 +0900

    Update: 워크스페이스 홈 메뉴 리스트에서 회의실 정보 페이지 url 수정

--- src/pages/mypage/workspace/index.tsx
+++ src/pages/mypage/workspace/index.tsx
@@ -22,7 +22,7 @@ const WorkspaceHome = () => {
       icon: <UserGroupOutlined width={24} height={24} />,
       href: `/${commonUrl}/member`,
     },
-    { id: 2, name: '회의실 정보', icon: <BoxOutlined width={24} height={24} />, href: '' },
+    { id: 2, name: '회의실 정보', icon: <BoxOutlined width={24} height={24} />, href: `/${commonUrl}/congress-room` },
     { id: 3, name: '카테고리 정보', icon: <CategoryGroupOutlined width={24} height={24} />, href: '' },
   ];
 
