e2c0d76db469a3f787fd9ddaa0eeadd21248e24d
hyoni0817 <hyoni0817@naver.com>
2024-12-02 15:33:02 +0900

    Feat: props에 배경색을 전달해서 메인 레이아웃 배경색이 변경되게 구현

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,25 +1,28 @@
 import React, { FC } from 'react';
 import stylex from '@stylexjs/stylex';
+import { colors } from '../../public/styles/vars.stylex';
 
 type MainLayoutProps = {
   isScroll?: boolean; // 전체 화면에 스크롤이 적용되게 할지 말지 결정
+  backgroundColor?: string;
   children: React.ReactElement | React.ReactElement[] | string;
 };
 
 const MainLayout: FC<MainLayoutProps> = (props) => {
-  const { isScroll, children } = props;
+  const { isScroll, backgroundColor, children } = props;
 
-  return <div {...stylex.props(Styles.container(isScroll))}>{children}</div>;
+  return <div {...stylex.props(Styles.container(isScroll, backgroundColor))}>{children}</div>;
 };
 
 export default MainLayout;
 
 const Styles = stylex.create({
-  container: (isScroll) => ({
+  container: (isScroll: boolean, backgroundColor: string) => ({
     width: '100%',
     height: '100%',
     margin: '0 auto',
     maxWidth: '767px',
     overflowY: isScroll ? 'auto' : 'hidden',
+    background: backgroundColor || colors.white500,
   }),
 });
