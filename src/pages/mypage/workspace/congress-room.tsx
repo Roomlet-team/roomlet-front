74001f66ae0d2a218a6c95265fe47e91e412dc85
hyoni0817 <hyoni0817@naver.com>
2024-12-02 15:38:27 +0900

    Feat: '회의실 추가' 버튼 추가

--- src/pages/mypage/workspace/congress-room.tsx
+++ src/pages/mypage/workspace/congress-room.tsx
@@ -8,6 +8,8 @@ import { colors, Typography } from '../../../../public/styles/vars.stylex';
 import { useDispatch, useSelector } from 'react-redux';
 import { saveCongressRoomList } from '@src/features/mypage/workspace/congress-room/slice/congressRoom';
 import { RootState } from '@src/store';
+import PlusOutlined from '@src/components/icons/PlusOutlined';
+import MainLayout from '@src/layouts/MainLayout';
 
 const MeetingRoom = () => {
   const { data } = useGetCongressRoomListQuery();
@@ -30,39 +32,71 @@ const MeetingRoom = () => {
   }, [isEdit]);
 
   return (
-    <GnbNavLayout backgroundColor="#FAFAFA">
+    <MainLayout backgroundColor="#FAFAFA">
       <Header title="회의실" prevUrl="/mypage/workspace" rightBtnInfo={completeBtnProps} />
 
-      {/* 회의실 개수 */}
-      <div {...stylex.props(Styles.TotalCountWrapper, Typography.TextSmallMedium)}>
-        전체 회의실 <span {...stylex.props(Styles.Count)}>{data?.congressRoomCount}</span>
-      </div>
+      <div {...stylex.props(Styles.Container)}>
+        {/* 회의실 개수 */}
+        <div {...stylex.props(Styles.TotalCountWrapper, Typography.TextSmallMedium)}>
+          전체 회의실 <span {...stylex.props(Styles.Count)}>{data?.congressRoomCount}</span>
+        </div>
 
-      {/* 회의실 목록 */}
-      <div {...stylex.props(Styles.RoomListWrapper)}>
-        {isEdit
-          ? // 수정 상태 일 때
-            editCongressRoomList?.map((item) => <CongressRoomCard data={item} isEdit={isEdit} />)
-          : // 수정 상태가 아닐 때
-            data?.congressRoomList.map((item) => <CongressRoomCard data={item} />)}
+        {/* 회의실 목록 */}
+        <div {...stylex.props(Styles.RoomListWrapper)}>
+          {isEdit
+            ? // 수정 상태 일 때
+              editCongressRoomList?.map((item) => <CongressRoomCard data={item} isEdit={isEdit} />)
+            : // 수정 상태가 아닐 때
+              data?.congressRoomList?.map((item) => <CongressRoomCard data={item} />)}
+        </div>
+
+        {/* 회의실 추가 */}
+        {isEdit && (
+          <button type="button" {...stylex.props(Styles.AddCongressRoomBtn, Typography.TextSmallMedium)}>
+            <PlusOutlined width={24} height={24} />
+            <span>회의실 추가</span>
+          </button>
+        )}
       </div>
-    </GnbNavLayout>
+    </MainLayout>
   );
 };
 
 export default MeetingRoom;
 
 const Styles = stylex.create({
-  TotalCountWrapper: {
+  Container: {
+    width: '100%',
+    height: 'calc(100vh - 60px)',
+    position: 'relative',
     padding: '16px',
+    marginBottom: '24px',
+    overflowY: 'auto',
+  },
+  TotalCountWrapper: {
+    paddingBottom: '16px',
   },
   RoomListWrapper: {
-    padding: '0 16px',
+    height: 'calc(100vh - 212px)',
+    marginBottom: '24px',
     display: 'flex',
     gap: '12px',
     flexDirection: 'column',
   },
   Count: {
     color: colors.red500,
+  },
+  AddCongressRoomBtn: {
+    width: '100%',
+    padding: '16px',
+    position: 'sticky',
+    bottom: '8px',
+    display: 'flex',
+    gap: '8px',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: colors.red500,
+    color: colors.white500,
+    borderRadius: '20px',
   },
 });
