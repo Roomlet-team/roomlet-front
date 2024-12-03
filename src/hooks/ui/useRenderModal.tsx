8b1df20649fdcc2fd5997a56de05442fe4232865
hyoni0817 <hyoni0817@naver.com>
2024-12-04 02:00:12 +0900

    Update: useCreateModal에서 useRenderModal로 변경
    - useDispatch 불러올 떄 Provider로 컴포넌트가 감싸져있지 않다는 오류가 발생함.
    
    - root.render를 사용한 경우에는 _app.tsx가 아닌 별도의 화면으로 렌더링 되어서 _app.tsx에서 사용한 Provider가 사용되지 않음.
    
    - root.render 내에서 <Provider>를 또 사용해주면 메모리 낭비 및 누수가 발생할 문제가 생겨서 redux-toolkit을 사용하여 모달 화면을 로드할 수 있도록 코드를 변경해줌.

---
+++ src/hooks/ui/useRenderModal.tsx
@@ -0,0 +1,29 @@
+import { useDispatch } from 'react-redux';
+import { hideModal, showModal } from '@src/slices/modal';
+
+type ReturnType = {
+  renderModal: (
+    Component: React.ReactElement | React.ReactElement[] | string,
+    props: { [key in string]: any }
+  ) => () => void;
+};
+
+/**
+ * Modal 및 BottomSheet 가 MainLayout 컴포넌트 안에서 render되게 하는 custom hook
+ * @returns renderModal 함수 반환
+ */
+function useRenderModal(): ReturnType {
+  const dispatch = useDispatch();
+
+  const renderModal = (Component, props) => {
+    dispatch(showModal(<Component {...props} />));
+
+    return () => {
+      dispatch(hideModal());
+    };
+  };
+
+  return { renderModal };
+}
+
+export default useRenderModal;
