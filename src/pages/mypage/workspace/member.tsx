c27137470d2e1523de284107a451b0cf486f9c3d
hyoni0817 <hyoni0817@naver.com>
2024-12-04 23:20:38 +0900

    Feat: 팀 추가 버튼 구현

--- src/pages/mypage/workspace/member.tsx
+++ src/pages/mypage/workspace/member.tsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import stylex from '@stylexjs/stylex';
-import GnbNavLayout from '@src/layouts/GnbNavLayout';
 import Header from '@src/components/ui/Header';
-import { Typography } from '../../../../public/styles/vars.stylex';
+import { colors, Typography } from '../../../../public/styles/vars.stylex';
 import TeamToggle from '@src/features/mypage/workspace/member/components/TeamToggle';
 import useGetTeamListQuery from '@src/queries/team/useGetTeamListQuery';
-import { useDispatch, useSelector } from 'react-redux';
 import { saveTeamList } from '@src/features/mypage/workspace/member/slices/member';
 import { RootState } from '@src/store';
+import PlusOutlined from '@src/components/icons/PlusOutlined';
+import useRenderModal from '@src/hooks/ui/useRenderModal';
+import MainLayout from '@src/layouts/MainLayout';
+import AddTeamBottomSheet from '@src/features/mypage/workspace/member/components/AddTeamBottomSheet';
 
 const Member = () => {
   const { data } = useGetTeamListQuery();
   const dispatch = useDispatch();
   const { editTeamList } = useSelector((state: RootState) => state.member);
   const [isEdit, setIsEdit] = useState<boolean>(false);
+  const { renderModal } = useRenderModal();
 
   const totalMemberCount = data?.teamList?.reduce(
     (prevValue, currentValue) => prevValue + currentValue?.memberList?.length,
@@ -28,6 +32,10 @@ const Member = () => {
     },
   };
 
+  const handleClickAddTeam = () => {
+    renderModal(AddTeamBottomSheet, null);
+  };
+
   useEffect(() => {
     if (isEdit) {
       dispatch(saveTeamList(data.teamList));
@@ -35,32 +43,70 @@ const Member = () => {
   }, [isEdit]);
 
   return (
-    <GnbNavLayout>
+    <MainLayout>
       <Header title="멤버" prevUrl="/mypage/workspace" rightBtnInfo={completeBtnProps} />
       {/* 검색 */}
 
-      {/* 전체 멤버수 */}
-      <div {...stylex.props(Styles.TotalCountWrapper, Typography.TextSmallMedium)}>전체 멤버 ({totalMemberCount})</div>
+      <div {...{ ...stylex.props(Styles.Container) }}>
+        {/* 전체 멤버수 */}
+        <div {...stylex.props(Styles.TotalCountWrapper, Typography.TextSmallMedium)}>
+          전체 멤버 ({totalMemberCount})
+        </div>
 
-      {/* 팀 목록 */}
-      <div {...stylex.props(Styles.TeamListContainer)}>
-        {isEdit
-          ? editTeamList?.map((item) => <TeamToggle key={item.TeamId} data={item} isEdit={isEdit} />)
-          : data?.teamList?.map((item) => <TeamToggle key={item.TeamId} data={item} />)}
+        {/* 팀 목록 */}
+        <div {...stylex.props(Styles.TeamListContainer)}>
+          {isEdit
+            ? editTeamList?.map((item) => <TeamToggle key={item.TeamId} data={item} isEdit={isEdit} />)
+            : data?.teamList?.map((item) => <TeamToggle key={item.TeamId} data={item} />)}
+        </div>
+
+        {/* 팀 추가 */}
+        {isEdit && (
+          <button
+            type="button"
+            {...stylex.props(Styles.AddTeamBtn, Typography.TextSmallMedium)}
+            onClick={handleClickAddTeam}
+          >
+            <PlusOutlined width={24} height={24} />
+            <span>팀 추가</span>
+          </button>
+        )}
       </div>
-    </GnbNavLayout>
+    </MainLayout>
   );
 };
 
 export default Member;
 
 const Styles = stylex.create({
+  Container: {
+    width: '100%',
+    height: 'calc(100vh - 60px)',
+    position: 'relative',
+    padding: '16px',
+    marginBottom: '24px',
+    overflowY: 'auto',
+  },
   TotalCountWrapper: {
     padding: '16px',
   },
   TeamListContainer: {
     width: '100%',
+    height: 'calc(100vh - 212px)',
     display: 'flex',
     flexDirection: 'column',
+  },
+  AddTeamBtn: {
+    width: '100%',
+    padding: '16px',
+    position: 'sticky',
+    bottom: '8px',
+    display: 'flex',
+    gap: '8px',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: colors.red500,
+    color: colors.white500,
+    borderRadius: '20px',
   },
 });
