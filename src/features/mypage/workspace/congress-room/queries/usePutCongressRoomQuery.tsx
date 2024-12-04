592a601dfbbce6fa863a54aedc3f6a88daddc2c3
hyoni0817 <hyoni0817@naver.com>
2024-12-04 18:12:16 +0900

    Feat: React-query를 사용하여 워크스페이스 회의실 수정하는 커스텀 훅 구현

---
+++ src/features/mypage/workspace/congress-room/queries/usePutCongressRoomQuery.tsx
@@ -0,0 +1,61 @@
+import { confirm } from '@src/components/ui/Modal/confirm';
+import useCustomMutation from '@src/hooks/react-query/useCustomMutation';
+import useGetWorkspaceListQuery from '@src/queries/workspace/useGetWorkspaceListQuery';
+import clientInstance from '@src/utils/api/clientInstance';
+import { useQueryClient } from '@tanstack/react-query';
+
+type ResponseData = null;
+
+interface CongressRoomListInfo {
+  congressRoomList: {
+    RoomId: number;
+    roomName: string;
+    roomDescription: string;
+    roomSize: number;
+  }[];
+  deleteCongressRoomList: {
+    RoomId: number;
+    roomName: string;
+    roomDescription: string;
+    roomSize: number;
+  }[];
+}
+
+const putCongressRoomApi = async (WorkspaceId, data: CongressRoomListInfo): ResponseData => {
+  const response = await clientInstance.put(`/v1/workspace/@${WorkspaceId}/congress/room`, data);
+
+  return response.data;
+};
+
+/**
+ * React-query를 사용하여 워크스페이스 회의실 수정하는 커스텀 훅
+ * @returns
+ * - 다음 객체를 반환합니다:
+ *   - `data`: 회의실 데이터 수정 후 반환하는 응답데이터 없음
+ *   - `error`: 에러 발생 시 에러 객체
+ *   - `isIdle`: 쿼리 비활성화 여부 (enabled가 true 이고 쿼리 불러오기가 시작될 때까지 isIdle 은 true)
+ *   - `isPending`: mutation 실행중인지에 대한 여부
+ *   - `isError`: 에러가 발생했는지에 대한 여부
+ *   - `isSuccess`: mutation이 성공했고, mutation data를 사용할 수 있는지에 대한 여부
+ */
+const usePutCongressRoomQuery = () => {
+  const { data } = useGetWorkspaceListQuery();
+  const queryClient = useQueryClient();
+  const workspaceId = data?.workspaceList[0]?.WorkspaceId;
+
+  const mutation = useCustomMutation({
+    mutationFn: (data: CongressRoomListInfo) => putCongressRoomApi(workspaceId, data),
+    onSuccess: (data, variables) => {
+      // 업데이트 후 회의실 리스트 데이터 재요청 (쿼리 무효화)
+      queryClient.invalidateQueries({ queryKey: ['roomList'] });
+    },
+    onError: (error, variables, context) => {
+      // 에러가 발생한 경우, 에러 내용이 담긴 confirm 모달 띄우기
+      confirm({ content: error?.response.data.message.errMsg });
+    },
+  });
+
+  return mutation;
+};
+
+export default usePutCongressRoomQuery;
