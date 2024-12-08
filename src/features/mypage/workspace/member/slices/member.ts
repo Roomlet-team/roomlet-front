e2bdbe744783f37043863296d8b008008bc05d81
hyoni0817 <hyoni0817@naver.com>
2024-12-08 15:44:56 +0900

    Feat: 팀 제거 리듀서 구현

--- src/features/mypage/workspace/member/slices/member.ts
+++ src/features/mypage/workspace/member/slices/member.ts
@@ -3,11 +3,14 @@ import { EditTeamItem } from '../types/member';
 
 export interface MemberState {
   editTeamList: EditTeamItem[];
+  tempDeleteTeamList: EditTeamItem[];
 }
 
 const initialState: MemberState = {
   // 팀 리스트 저장
   editTeamList: [],
+  // 임시 팀 제거 리스트 저장
+  tempDeleteTeamList: [],
 };
 
 export const MemberSlice = createSlice({
@@ -25,10 +28,40 @@ export const MemberSlice = createSlice({
 
       state.editTeamList = [...state.editTeamList, { ...action.payload, tempTeamId, memberList: [] }];
     },
+    // 팀 제거
+    tempRemoveTeam: (state, action) => {
+      // 팀 제거 전, 미분류 팀으로 모두 이동
+      const moveMemberTeamList = state.editTeamList.map((item) => {
+        let memberList = item.memberList;
+
+        // [ ] teamType이 base로 설정되기 전까지 미분류 이름으로 나눌 수 있게 수정
+        if (item.teamType === 'base' || item.teamName === '미분류') {
+          memberList = [...item.memberList, ...action.payload.memberList];
+        }
+
+        return { ...item, memberList };
+      });
+
+      // 팀 제거 시, 임시로 생성한 팀과 기존 팀을 별도로 처리해서 제거
+      const filterRemoveTeamList = moveMemberTeamList.filter((item) => {
+        if (item.TeamId) {
+          return item.TeamId !== action.payload.TeamId;
+        }
+
+        return item.tempTeamId !== action.payload.tempTeamId;
+      });
+
+      // TeamId가 존재하는 경우에만 팀 제거 리스트에 추가
+      if (action.payload.TeamId) {
+        state.tempDeleteTeamList = [...state.tempDeleteTeamList, action.payload];
+      }
+
+      state.editTeamList = filterRemoveTeamList;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { saveTeamList, addTeam } = MemberSlice.actions;
+export const { saveTeamList, addTeam, tempRemoveTeam } = MemberSlice.actions;
 
 export default MemberSlice.reducer;
