1a6de08f3bbe2bfc61897e6c3842531a25574915
hyoni0817 <hyoni0817@naver.com>
2024-12-16 23:12:48 +0900

    Feat: 회의실 카테고리 추가 리듀서 구현

--- src/features/mypage/workspace/category/slices/category.ts
+++ src/features/mypage/workspace/category/slices/category.ts
@@ -18,10 +18,21 @@ export const CategorySlice = createSlice({
     saveCategoryList: (state, action) => {
       state.editCongressCategoryList = action.payload;
     },
+    // 회의실 카테고리 추가
+    addCategory: (state, action) => {
+      // 임시로 생성한 카테고리를 제거할 때, 고유 id 값으로 판별하기 위해 tempCongressCategoryId 생성
+      const tempCongressCategoryId =
+        state.editCongressCategoryList.filter((item) => item.tempCongressCategoryId).length + 1;
+
+      state.editCongressCategoryList = [
+        ...state.editCongressCategoryList,
+        { ...action.payload, tempCongressCategoryId },
+      ];
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { saveCategoryList } = CategorySlice.actions;
+export const { saveCategoryList, addCategory } = CategorySlice.actions;
 
 export default CategorySlice.reducer;
