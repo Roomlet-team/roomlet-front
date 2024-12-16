33cf5c3e1cb884855d2d4eb715260185cefd5362
hyoni0817 <hyoni0817@naver.com>
2024-12-16 23:12:16 +0900

    Feat: 카테고리 추가 Bottom sheet 구현

---
+++ src/features/mypage/workspace/category/components/AddCategoryBottomSheet/index.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import stylex from '@stylexjs/stylex';
+import BottomSheet from '@src/components/ui/BottomSheet';
+import { colors, Typography } from '../../../../../../../public/styles/vars.stylex';
+import Input from '@src/components/ui/Input';
+import useInput from '@src/hooks/useInput';
+import { hideModal } from '@src/slices/modal';
+import { addCategory } from '../../slices/category';
+
+let bottomSheetId = 'add-category-bottom-sheet';
+
+const AddCategoryBottomSheet = () => {
+  const [name, handleChangeName] = useInput<string>('');
+  const dispatch = useDispatch();
+
+  const handleClickRegister = () => {
+    dispatch(addCategory({ categoryName: name }));
+    dispatch(hideModal());
+  };
+
+  return (
+    <BottomSheet id={bottomSheetId} onClick={() => dispatch(hideModal())}>
+      <div {...stylex.props(Styles.Container)}>
+        <p {...stylex.props(Typography.SubtitleRegularSemiBold, Styles.Title)}>카테고리 추가</p>
+        <div {...stylex.props(Styles.InputContainer)}>
+          <Input type="text" placeholder="카테고리 명을 입력해주세요" value={name} onChange={handleChangeName} />
+        </div>
+        <div {...stylex.props(Styles.ButtonContainer)}>
+          <button
+            type="button"
+            onClick={() => dispatch(hideModal())}
+            {...stylex.props(Styles.Button, Styles.CancelButton, Typography.TextSmallMedium)}
+          >
+            닫기
+          </button>
+          <button
+            type="button"
+            onClick={handleClickRegister}
+            {...stylex.props(Styles.Button, Styles.RegisterButton, Typography.TextSmallMedium)}
+          >
+            등록
+          </button>
+        </div>
+      </div>
+    </BottomSheet>
+  );
+};
+
+export default AddCategoryBottomSheet;
+
+const Styles = stylex.create({
+  Container: {
+    padding: '24px 16px',
+  },
+  Title: {
+    marginBottom: '24px',
+    textAlign: 'center',
+  },
+  InputContainer: {
+    marginBottom: '24px',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '12px',
+  },
+  ButtonContainer: {
+    display: 'flex',
+    gap: '8px',
+    justifyContent: 'space-between',
+  },
+  Button: {
+    padding: '16px',
+    borderRadius: '20px',
+    flex: 1,
+  },
+  CancelButton: {
+    background: colors.gray20,
+    color: colors.gray60,
+  },
+  RegisterButton: {
+    background: colors.red500,
+    color: colors.white500,
+  },
+});
