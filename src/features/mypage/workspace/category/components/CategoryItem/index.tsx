93496c15b09d0b8055a3ccf4cad35570b6307044
hyoni0817 <hyoni0817@naver.com>
2024-12-18 23:38:07 +0900

    Feat: 회의실 카테고리 제거 버튼 구현

--- src/features/mypage/workspace/category/components/CategoryItem/index.tsx
+++ src/features/mypage/workspace/category/components/CategoryItem/index.tsx
@@ -5,17 +5,34 @@ import { CongressCategoryItem } from '@src/queries/category/useGetCategoryListQu
 import useInput from '@src/hooks/useInput';
 import CircleCloseFilled from '@src/components/icons/CircleCloseFilled';
 import { colors, Typography } from '../../../../../../../public/styles/vars.stylex';
+import { confirm } from '@src/components/ui/Modal/confirm';
+import { useDispatch } from 'react-redux';
+import { tempRemoveCategory } from '../../slices/category';
+import { editCongressCategoryItem } from '../../types/category';
 
 interface CategoryItemProps {
-  data: CongressCategoryItem;
+  data: editCongressCategoryItem;
   isEdit?: boolean;
 }
 
 const CategoryItem: FC<CategoryItemProps> = (props) => {
   const { isEdit, data } = props;
   const [categoryName, handleChangeCategoryName] = useInput<string>(data?.categoryName);
+  const dispatch = useDispatch();
 
-  const handleClickDelete = () => {};
+  const handleClickDelete = () => {
+    confirm({
+      content: '기존 회의 기록은 변하지 않지만,\n새로운 회의에서는 더이상 사용할 수 없어요.',
+      cancelBtnName: '유지할래요',
+      okBtnName: '삭제할래요',
+      onOk: () =>
+        dispatch(
+          tempRemoveCategory(
+            data.tempCongressCategoryId ? { tempCongressCategoryId: data.tempCongressCategoryId } : { ...data }
+          )
+        ),
+    });
+  };
 
   return (
     <div {...stylex.props(Styles.Container)}>
