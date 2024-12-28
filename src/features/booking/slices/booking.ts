c925876c6bf028db166d19cb77a2ff8b034cb1d7
hyoni0817 <hyoni0817@naver.com>
2024-12-29 03:31:48 +0900

    Feat: 선택한 회의 참석자를 저장하는 리듀서 구현

--- src/features/booking/slices/booking.ts
+++ src/features/booking/slices/booking.ts
@@ -1,24 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { MemberInfoItem } from '@src/queries/team/useGetTeamListQuery';
 
 export interface BookingState {
   selectBookingDate: string;
+  selectBookingMemberObj: { [teamName in string]: MemberInfoItem[] };
 }
 
 const initialState: BookingState = {
   selectBookingDate: '',
+  selectBookingMemberObj: {},
 };
 
 export const BookingSlice = createSlice({
   name: 'booking',
   initialState,
   reducers: {
+    // 선택한 회의 예약 날짜
     saveSelectBookingDate: (state, action) => {
       state.selectBookingDate = action.payload;
+    },
+    // 선택한 참석자를 모은 객체
+    saveSelectBookingMemberObj: (state, action) => {
+      state.selectBookingMemberObj = action.payload;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { saveSelectBookingDate } = BookingSlice.actions;
+export const { saveSelectBookingDate, saveSelectBookingMemberObj } = BookingSlice.actions;
 
 export default BookingSlice.reducer;
