d29f8904d4fa3d126c8494aa56866413b16d792e
hyoni0817 <hyoni0817@naver.com>
2024-12-29 11:11:19 +0900

    Feat: React-query를 사용하여 회의를 생성하는 커스텀 훅 구현

---
+++ src/features/booking/queries/usePostWorkspaceCongressQuery.tsx
@@ -0,0 +1,56 @@
+import { confirm } from '@src/components/ui/Modal/confirm';
+import useCustomMutation from '@src/hooks/react-query/useCustomMutation';
+import useGetWorkspaceListQuery from '@src/queries/workspace/useGetWorkspaceListQuery';
+import clientInstance from '@src/utils/api/clientInstance';
+import { useRouter } from 'next/router';
+
+interface RequestBody {
+  RoomId: number;
+  CongressCategoryId: number;
+  congressTitle: string;
+  congressDescription: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  attendMemberList: {
+    MemberId: number;
+  }[];
+}
+
+const postCongressApi = async (WorkspaceId: number, data: RequestBody) => {
+  const response = await clientInstance.post(`/v1/workspace/@${WorkspaceId}/congress`, data);
+
+  return response.data;
+};
+
+/**
+ * React-query를 사용하여 회의를 생성하는 커스텀 훅
+ * @returns
+ * - 다음 객체를 반환합니다:
+ *   - `data`: 반환하는 데이터 없음 (아직 가져오지 않았다면 `undefined`)
+ *   - `error`: 에러 발생 시 에러 객체
+ *   - `isLoading`: 데이터 로딩 여부
+ *   - `refetch`: 데이터를 수동으로 다시 가져오는 함수
+ */
+const usePostWorkspaceCongressQuery = () => {
+  const { data } = useGetWorkspaceListQuery();
+  const router = useRouter();
+  const workspaceId = data?.workspaceList[0]?.WorkspaceId;
+
+  const result = useCustomMutation({
+    mutationFn: (data: RequestBody) => postCongressApi(workspaceId, data),
+    onSuccess: (data, variables, context) => {
+      router.push(`/reservations/1`);
+    },
+    onError: (error, variables, context) => {
+      if (error.response.data.code === 452) {
+        // 에러가 발생한 경우, 에러 내용이 담긴 confirm 모달 띄우기
+        confirm({ content: error?.response.data.message.errMsg });
+      }
+    },
+  });
+
+  return result;
+};
+
+export default usePostWorkspaceCongressQuery;
