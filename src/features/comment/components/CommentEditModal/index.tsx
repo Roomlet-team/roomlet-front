c231239eb958c51445fcbdac48ab2aefbd83b5bf
hyoni0817 <hyoni0817@naver.com>
2025-01-19 01:30:31 +0900

    Feat: 댓글 수정 모달 컴포넌트 구현

---
+++ src/features/comment/components/CommentEditModal/index.tsx
@@ -0,0 +1,61 @@
+import React, { FC } from 'react';
+import Header from '@src/components/ui/Header';
+import Modal from '@src/components/ui/Modal';
+import { CommentListItem } from '../../queries/useGetWorkspaceCongressCommentQuery';
+import stylex from '@stylexjs/stylex';
+import { colors } from '../../../../../public/styles/vars.stylex';
+import { useDispatch } from 'react-redux';
+import { hideModal } from '@src/slices/modal';
+import useTextArea from '@src/hooks/useTextArea';
+
+interface CommentEditModalProps {
+  data: CommentListItem;
+}
+
+const CommentEditModal: FC<CommentEditModalProps> = (props) => {
+  const { data } = props;
+  const [content, handleChangeContent] = useTextArea(data.content);
+  const dispatch = useDispatch();
+
+  const prevOnClick = () => {
+    dispatch(hideModal());
+  };
+
+  return (
+    <Modal isOpen>
+      <div {...stylex.props(Styles.container)}>
+        <Header
+          title="댓글 수정"
+          prevOnClick={prevOnClick}
+          rightBtnInfo={{ name: '완료', isActive: true, onClick: () => {} }}
+        />
+
+        {/* 수정할 댓글 내용 */}
+        <textarea value={content} onChange={handleChangeContent} {...stylex.props(Styles.textarea)} />
+      </div>
+    </Modal>
+  );
+};
+
+export default CommentEditModal;
+
+const Styles = stylex.create({
+  container: {
+    maxWidth: '100%',
+    height: '100vh',
+    background: colors.white500,
+  },
+  textarea: {
+    width: '100%',
+    height: '100%',
+    padding: '12px 28px',
+    background: colors.white500,
+    border: 'none',
+    outline: 'none',
+    resize: 'none',
+    fontSize: '1.4rem',
+    lineHeight: '2rem',
+    fontWeight: '400',
+    color: '#333333',
+  },
+});
