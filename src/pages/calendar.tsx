e9ce9f4bb25058266ceefd36928c37b3eef42426
hyoni0817 <hyoni0817@naver.com>
2025-02-16 00:24:48 +0900

    Feat: 캘린더에서 선택한 날짜가 MeetingSchedule 컴포넌트에 전달되게 구현

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import stylex from '@stylexjs/stylex';
 import GnbNavLayout from '@src/layouts/GnbNavLayout';
 import MonthlyCalendar from '@src/features/calendar/components/MonthlyCalendar';
 import MeetingSchedule from '@src/features/calendar/components/MeetingSchedule';
 import CirclePlusFilled from '@src/components/icons/CirclePlusFilled';
 import Link from 'next/link';
+import dayjs from 'dayjs';
 
 const Calendar = () => {
+  const [selectDate, setSelectDate] = useState<string>(dayjs().format('YYYY-MM-DD'));
+
+  const handleSelectDate = (date: string) => {
+    setSelectDate(date);
+  };
+
   return (
     <GnbNavLayout>
       {/* 월간 캘린더 */}
-      <MonthlyCalendar />
+      <MonthlyCalendar onSelectDate={handleSelectDate} />
       {/* 캘린더에서 선택한 날짜에 대한 회의 리스트 보는 영역 */}
-      <MeetingSchedule />
+      <MeetingSchedule selectDate={selectDate} />
 
       {/* 예약하기 링크 (플로팅) */}
       <div {...stylex.props(Styles.CreateReservationWrapper)}>
