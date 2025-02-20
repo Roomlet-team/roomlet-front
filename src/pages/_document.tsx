ec83a20a6fa7ec4265a6353ecfe9d2ffdacdc06d
hyoni0817 <hyoni0817@naver.com>
2025-02-20 23:46:30 +0900

    Update: @ant-design/cssinjs를 제거함으로써 사용하지 않는 코드 제거

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import type { DocumentContext } from 'next/document';
 
@@ -14,27 +13,10 @@ const MyDocument = () => (
 );
 
 MyDocument.getInitialProps = async (ctx: DocumentContext) => {
-  const cache = createCache();
-  const originalRenderPage = ctx.renderPage;
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) => (
-        <StyleProvider cache={cache}>
-          <App {...props} />
-        </StyleProvider>
-      ),
-    });
-
   const initialProps = await Document.getInitialProps(ctx);
-  const style = extractStyle(cache, true);
   return {
     ...initialProps,
-    styles: (
-      <>
-        {initialProps.styles}
-        <style dangerouslySetInnerHTML={{ __html: style }} />
-      </>
-    ),
+    styles: <>{initialProps.styles}</>,
   };
 };
 
