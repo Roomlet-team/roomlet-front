5aa76d65bb6fb270d7392e9daa14fcf43671c268
hyoni0817 <hyoni0817@naver.com>
2025-02-21 00:17:37 +0900

    Update: js 크기 최적화
    - 빌드 시 First Load JS 크기가 큰 페이지들을 체크하여, dynamic import()로 코드 스플리팅 진행함.

--- src/pages/reservations/[id].tsx
+++ src/pages/reservations/[id].tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import stylex from '@stylexjs/stylex';
 import { colors, Typography } from '../../../public/styles/vars.stylex';
 import MainLayout from '@src/layouts/MainLayout';
-import CommentTextarea from '@src/features/comment/components/CommentTextarea';
-import CommentList from '@src/features/comment/components/CommentList';
-import ReservationInfo from '@src/features/reservation/components/ReservationInfo';
+// import CommentTextarea from '@src/features/comment/components/CommentTextarea';
+// import CommentList from '@src/features/comment/components/CommentList';
+// import ReservationInfo from '@src/features/reservation/components/ReservationInfo';
 import Header from '@src/components/ui/Header';
+import dynamic from 'next/dynamic';
+
+const CommentList = dynamic(() => import('@src/features/comment/components/CommentList'), {
+  ssr: false,
+});
+const CommentTextarea = dynamic(() => import('@src/features/comment/components/CommentTextarea'), {
+  ssr: false,
+});
+const ReservationInfo = dynamic(() => import('@src/features/reservation/components/ReservationInfo'), {
+  ssr: false,
+});
 
 const MeetingDetails = () => {
   return (
