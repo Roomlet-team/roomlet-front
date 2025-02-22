2cead332e02fc2fc9dcf7dccdcb3c58f88c22ffc
hyoni0817 <hyoni0817@naver.com>
2025-02-22 23:22:31 +0900

    Update: 데이터를 props로 전달받아서 로드하는 방식으로 수정

--- src/features/mypage/compontents/MypageSummaryProfile/index.tsx
+++ src/features/mypage/compontents/MypageSummaryProfile/index.tsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { FC } from 'react';
+import Link from 'next/link';
 import stylex from '@stylexjs/stylex';
 import ProfileImg from '@src/components/ui/ProfileImg';
 import { Typography, colors } from '../../../../../public/styles/vars.stylex';
 import ArrowHeadOutlinedV2 from '@src/components/icons/ArrowHeadOutlinedV2';
-import Link from 'next/link';
-import useGetMypageInfoQuery from '../../queries/useGetMypageInfoQuery';
 
-const MypageSummaryProfile = () => {
-  const { data } = useGetMypageInfoQuery();
+interface MypageSummaryProfileProps {
+  data: {
+    displayName: string;
+    profileImgUrl: string | null;
+    email: string;
+  };
+}
+
+const MypageSummaryProfile: FC<MypageSummaryProfileProps> = (props) => {
+  const { data } = props;
 
   return (
     <div {...stylex.props(Styles.Container)}>
       <Link href="/mypage/profile" {...stylex.props(Styles.ProfileLink)} passHref>
-        <ProfileImg src={data?.myInfo.profileImgUrl} size={60} />
+        <ProfileImg src={data?.profileImgUrl} size={60} />
         <div {...stylex.props(Styles.UserInfoContainer)}>
           <div>
-            <p {...stylex.props(Styles.NameText, Typography.SubTextRegularBold)}>{data?.myInfo.displayName}</p>
-            <p {...stylex.props(Styles.emailText, Typography.SubTextLargeRegular)}>{data?.myInfo.email}</p>
+            <p {...stylex.props(Styles.NameText, Typography.SubTextRegularBold)}>{data?.displayName}</p>
+            <p {...stylex.props(Styles.emailText, Typography.SubTextLargeRegular)}>{data?.email}</p>
           </div>
           <ArrowHeadOutlinedV2 width={24} height={24} color={colors.gray60} />
         </div>
