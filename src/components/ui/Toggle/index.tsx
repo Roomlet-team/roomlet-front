b9b65473438f0586a71b8df33d2de173c1626c8f
hyoni0817 <hyoni0817@naver.com>
2025-03-02 23:56:38 +0900

    Feat: 지정한 theme에 맞게 토글 색상이 변경되도록 구현

--- src/components/ui/Toggle/index.tsx
+++ src/components/ui/Toggle/index.tsx
@@ -3,12 +3,13 @@ import styles from './styles/styles.module.css';
 
 type ToggleProps = {
   onChange: (e: React.ChangeEvent) => void;
+  theme?: 'red500' | 'black400';
 };
 
 const Toggle: FC<ToggleProps> = (props) => {
-  const { onChange } = props;
+  const { onChange, theme = 'red500' } = props;
 
-  return <input className={styles['common-toggle']} type="checkbox" onChange={onChange} />;
+  return <input className={`${styles['common-toggle']} ${styles[theme]}`} type="checkbox" onChange={onChange} />;
 };
 
 export default Toggle;
