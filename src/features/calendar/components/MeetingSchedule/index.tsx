20ac5eaf382c1683737ac20f759b38814afa214e
hyoni0817 <hyoni0817@naver.com>
2025-03-03 01:40:06 +0900

    Feat: 캘린더에서 회의 스케줄 리스트에 회의가 없는 경우 문구가 표시되게 구현

--- src/features/calendar/components/MeetingSchedule/index.tsx
+++ src/features/calendar/components/MeetingSchedule/index.tsx
@@ -3,6 +3,7 @@ import stylex from '@stylexjs/stylex';
 import useGetWorkspaceCongressListQuery from '@src/queries/workspace/useGetWorkspaceCongressListQuery';
 import dayjs from 'dayjs';
 import router from 'next/router';
+import { colors } from 'public/styles/vars.stylex';
 
 const MeetingSchedule: FC<{ selectDate: string }> = ({ selectDate }) => {
   const { data } = useGetWorkspaceCongressListQuery({
@@ -17,22 +18,33 @@ const MeetingSchedule: FC<{ selectDate: string }> = ({ selectDate }) => {
     <div {...stylex.props(Styles.container)}>
       <h3 {...stylex.props(Styles.selectedDateTitle)}>{dayjs(selectDate).format('D.ddd')}</h3>
       <div {...stylex.props(Styles.meetingListContent)}>
-        {data?.congressList.map((item) => (
-          <div {...stylex.props(Styles.meetingContent)} onClick={() => handleMeetingClick(item.CongressId)}>
-            <div {...stylex.props(Styles.meetingCategoryLine(item.congressCategory.hexcode))} />
+        {data?.congressList.length > 0 ? (
+          data?.congressList.map((item) => (
+            <div {...stylex.props(Styles.meetingContent)} onClick={() => handleMeetingClick(item.CongressId)}>
+              <div {...stylex.props(Styles.meetingCategoryLine(item.congressCategory.hexcode))} />
+              <div {...stylex.props(Styles.detailContent)}>
+                <div {...stylex.props(Styles.infoContent)}>
+                  <p {...stylex.props(Styles.titleText('#333'))}>{item.congressTitle}</p>
+                  <p {...stylex.props(Styles.timeText)}>
+                    {item.startTime} ~ {item.endTime}
+                  </p>
+                </div>
+                <div {...stylex.props(Styles.categoryTag(item.congressCategory.hexcode))}>
+                  {item.congressCategory.categoryName}
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div {...stylex.props(Styles.meetingContent)}>
+            <div {...stylex.props(Styles.meetingCategoryLine(colors.gray40))} />
             <div {...stylex.props(Styles.detailContent)}>
               <div {...stylex.props(Styles.infoContent)}>
-                <p {...stylex.props(Styles.titleText)}>{item.congressTitle}</p>
-                <p {...stylex.props(Styles.timeText)}>
-                  {item.startTime} ~ {item.endTime}
-                </p>
-              </div>
-              <div {...stylex.props(Styles.categoryTag(item.congressCategory.hexcode))}>
-                {item.congressCategory.categoryName}
+                <p {...stylex.props(Styles.titleText('#7D7F85'))}>회의가 없습니다</p>
               </div>
             </div>
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
@@ -73,7 +85,7 @@ const Styles = stylex.create({
   }),
   detailContent: { width: '100%', display: 'flex', justifyContent: 'space-between' },
   infoContent: { display: 'flex', flexDirection: 'column', gap: '8px' },
-  titleText: { fontSize: '1.6rem', fontWeight: '500', lineHeight: '2.4rem' },
+  titleText: (color) => ({ fontSize: '1.6rem', fontWeight: '500', lineHeight: '2.4rem', color }),
   timeText: { fontSize: '1.4rem', fontWeight: '400', lineHeight: '2rem', color: 'var(--Gray-10)' },
   categoryTag: (hexcode) => ({
     padding: '4px 8px',
