8db5901481e5083b42ed46fa5b266ee789c97350
hyoni0817 <hyoni0817@naver.com>
2025-03-03 02:27:31 +0900

    Feat: theme 추가

--- src/components/ui/Radio/index.tsx
+++ src/components/ui/Radio/index.tsx
@@ -4,15 +4,16 @@ import styles from './styles/styles.module.css';
 interface RadioProps extends React.InputHTMLAttributes<HTMLInputElement> {
   key?: React.Key;
   label: string;
+  theme?: 'red500' | 'white';
 }
 
 const Radio: FC<RadioProps> = (props) => {
-  const { id, label, type, ...anotherProps } = props;
+  const { id, label, type, theme = 'red500', ...anotherProps } = props;
 
   return (
     <>
-      <label className={styles['common-radio-label']} htmlFor={id}>
-        <input className={styles['common-radio-input']} type="radio" id={id} {...anotherProps} />
+      <label className={`${styles['common-radio-label']} ${styles[theme]}`} htmlFor={id}>
+        <input className={`${styles['common-radio-input']} ${styles[theme]}`} type="radio" id={id} {...anotherProps} />
         {label}
       </label>
     </>
