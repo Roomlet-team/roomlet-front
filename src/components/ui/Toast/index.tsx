a4e9ffc6794bc575cf946e060a83296843f59fc2
hyoni0817 <hyoni0817@naver.com>
2025-03-09 23:21:23 +0900

    Feat: 공통으로 사용할 토스트 메시지 컴포넌트 구현

---
+++ src/components/ui/Toast/index.tsx
@@ -0,0 +1,48 @@
+import React, { FC } from 'react';
+import { Slide, toast } from 'react-toastify';
+import styles from './styles/styles.module.css';
+import { Typography } from '../../../../public/styles/vars.stylex';
+import stylex from '@stylexjs/stylex';
+import CircleCheckmarkFilled from '@src/components/icons/CircleCheckmarkFilled';
+
+interface ToastProps {
+  message: string;
+}
+
+const MessageContent: FC<ToastProps> = (props) => {
+  const { message } = props;
+
+  return (
+    <div {...stylex.props(Styles.Message, Typography.TextSmallMedium)}>
+      <CircleCheckmarkFilled width={24} height={24} />
+      <span>{message}</span>
+    </div>
+  );
+};
+
+const Toast: FC<ToastProps> = (props) => {
+  const { message } = props;
+
+  return toast(<MessageContent message={message} />, {
+    position: 'bottom-center',
+    autoClose: 1500,
+    closeButton: false,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    className: styles['toast-container'],
+    transition: Slide, // Bounce, Slide, Zoom, Flip, Fade
+  });
+};
+
+export default Toast;
+
+const Styles = stylex.create({
+  Message: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '4px',
+  },
+});
