0e7331c1deef89e0d7a6ef6e5275d68e3b1d9a7d
hyoni0817 <hyoni0817@naver.com>
2025-03-22 15:42:38 +0900

    Feat: 상태코드에 따른 메시지가 출력되게 구현

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -20,9 +20,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       `refresh_token=deleted; path=/; domain=${process.env.NEXT_PUBLIC_COOKIE_DOMAIN}; httpOnly=${process.env.NEXT_PUBLIC_HTTP_ONLY}; secure=true;  Max-Age=0;`,
     ]);
 
-    return res.status(200).json({ message: 'success' });
+    return res.status(200).json({ success: true, code: 200, message: 'success' });
   } catch (e) {
     console.error(e);
-    return res.status(401).json({ message: 'Unauthorize' });
+
+    if (e.response.status === 401) {
+      return res
+        .status(401)
+        .json({ success: false, code: 401, message: { errMsg: '로그아웃 할 수 있는 권한이 없습니다.' } });
+    } else if (e.response.status === 403) {
+      return res.status(403).json({ success: false, code: 403, message: { errMsg: '접근할 수 없는 페이지입니다.' } });
+    } else {
+      return res.status(500).json({ success: false, code: 500, message: { errMsg: '서버 에러가 발생했습니다.' } });
+    }
   }
 };
