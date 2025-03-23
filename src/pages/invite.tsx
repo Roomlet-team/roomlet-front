f763ea8d6b8e1d3cb57245fcbdf5414612484ffb
hyoni0817 <hyoni0817@naver.com>
2025-03-23 13:20:51 +0900

    Update: 회의 생성 및 로그아웃을 했을 때 이전에 캐싱된 데이터를 가져오는 문제가 있어서 staleTime 주석 처리
    - staleTime이 필요한 곳에서만 해당 useQuery에 설정하기로 함.

--- src/pages/invite.tsx
+++ src/pages/invite.tsx
@@ -144,7 +144,7 @@ export const getServerSideProps: GetServerSideProps = async (context: GetServerS
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 60 * 1000,
+        // staleTime: 60 * 1000,
       },
     },
   });
