1325d26fd9a7ef564156a196c0f53b3eaf91f6cd
hyoni0817 <hyoni0817@naver.com>
2025-04-05 23:51:17 +0900

    Update: 알림 수신 설정 항목 변경

--- src/pages/mypage/alarm.tsx
+++ src/pages/mypage/alarm.tsx
@@ -8,20 +8,8 @@ const MyPageAlarmSetting = () => {
   const toggleList = [
     {
       id: 1,
-      label: '회의 초대',
-      caption: '새로운 회의에 초대되면 알려드려요.',
-      apiUrl: '',
-    },
-    {
-      id: 2,
-      label: '회의 변경',
-      caption: '참여중인 회의에 변경사항이 있을 때 알려드려요.',
-      apiUrl: '',
-    },
-    {
-      id: 3,
-      label: '회의 리마인드',
-      caption: '회의 시간이 다가오면 30분 전에 다시 한 번 알려드려요.',
+      label: '회의 알림',
+      caption: '회의 리마인드, 초대, 변경 사항이 있을 때 알려드려요.',
       apiUrl: '',
     },
   ];
