aa3221fdd130fd28d370641bbe7f7e1180803280
hyoni0817 <hyoni0817@naver.com>
2025-04-06 00:08:58 +0900

    Feat: checked 값을 props로 전달받도록 추가
    - 알림 해지 시 안내 팝업이 나타나는데, 그만 받을래요를 선택해야 알림이 해지될 수 있음.
    
    - checked를 전달받기 전에는 그만 받을래요를 선택하지 않아도 알림이 바로 해지되는 문제가 있었음.
    
    - 그래서 해당 문제를 해결하고자 checked를 props로 전달받아서 토글 상태를 표현하게 함.

--- src/components/ui/Toggle/index.tsx
+++ src/components/ui/Toggle/index.tsx
@@ -3,13 +3,21 @@ import styles from './styles/styles.module.css';
 
 type ToggleProps = {
   onChange: (e: React.ChangeEvent) => void;
+  checked?: boolean;
   theme?: 'red500' | 'black400';
 };
 
 const Toggle: FC<ToggleProps> = (props) => {
-  const { onChange, theme = 'red500' } = props;
+  const { onChange, theme = 'red500', checked } = props;
 
-  return <input className={`${styles['common-toggle']} ${styles[theme]}`} type="checkbox" onChange={onChange} />;
+  return (
+    <input
+      className={`${styles['common-toggle']} ${styles[theme]}`}
+      type="checkbox"
+      checked={checked}
+      onChange={onChange}
+    />
+  );
 };
 
 export default Toggle;
