0ff051939a4dd2e03ce00a57586a656dfe1a2055
hyoni0817 <hyoni0817@naver.com>
2025-05-18 03:37:12 +0900

    Feat: 알람 설정 데이터 로드 및 수정 api 연결
    - 원래 여러 개의 알람 상태를 설정할 수 있는 기획이어서, 알람 주제별로 apiUrl이 하나씩 존재할거라고 생각했음.
    
    - 근데 알람 설정을 하나만 하는 것으로 기획이 변경되어서 apiUrl을 삭제함.

--- src/features/mypage/compontents/MyPageAlarmToggle/index.tsx
+++ src/features/mypage/compontents/MyPageAlarmToggle/index.tsx
@@ -1,19 +1,21 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import stylex from '@stylexjs/stylex';
 import Toggle from '@src/components/ui/Toggle';
 import { Typography, colors } from '../.././../../../public/styles/vars.stylex';
 import { confirm } from '@src/components/ui/Modal/confirm';
+import usePatchWorkspaceMypageSettingsAlarmsQuery from '@src/features/alarm/queries/usePatchWorkspaceMypageSettingsAlarmsQuery';
 
 type MyPageAlarmToggleProps = {
   key: string | number;
   label: string;
   caption: string;
-  apiUrl: string;
+  value: boolean;
 };
 
 const MyPageAlarmToggle: FC<MyPageAlarmToggleProps> = (props) => {
-  const { label, caption, apiUrl } = props;
+  const { label, caption, value } = props;
   const [isInviteMeeting, setIsInviteMeeting] = useState<boolean>(false);
+  const { mutate: patchWorkspaceMypageSettingsAlarmsMutate } = usePatchWorkspaceMypageSettingsAlarmsQuery();
 
   const handleChangeAlarm = (e) => {
     const { checked } = e.target;
@@ -23,14 +25,22 @@ const MyPageAlarmToggle: FC<MyPageAlarmToggleProps> = (props) => {
         content: '중요한 알림을 놓칠 수 있어요.\n그래도 괜찮으신가요?',
         onOk: () => {
           setIsInviteMeeting(checked);
+          patchWorkspaceMypageSettingsAlarmsMutate({ isAlarm: checked });
         },
         okBtnName: '그만 받을래요',
         cancelBtnName: '계속 받을래요',
       });
     } else {
       setIsInviteMeeting(checked);
+      patchWorkspaceMypageSettingsAlarmsMutate({ isAlarm: checked });
     }
   };
+
+  useEffect(() => {
+    if (value) {
+      setIsInviteMeeting(value);
+    }
+  }, [value]);
 
   return (
     <div {...stylex.props(Styles.Container)}>
--- src/pages/mypage/alarm.tsx
+++ src/pages/mypage/alarm.tsx
@@ -1,16 +1,18 @@
+import React from 'react';
 import Header from '@src/components/ui/Header';
 import Toggle from '@src/components/ui/Toggle';
+import useGetWorkspaceMypageSettingsAlarmsQuery from '@src/features/alarm/queries/useGetWorkspaceMypageSettingsAlarmsQuery';
 import MyPageAlarmToggle from '@src/features/mypage/compontents/MyPageAlarmToggle';
 import MainLayout from '@src/layouts/MainLayout';
-import React, { useState } from 'react';
 
 const MyPageAlarmSetting = () => {
+  const { data } = useGetWorkspaceMypageSettingsAlarmsQuery();
   const toggleList = [
     {
       id: 1,
       label: '회의 알림',
       caption: '회의 리마인드, 초대, 변경 사항이 있을 때 알려드려요.',
-      apiUrl: '',
+      value: data?.alarmInfo.isAlarm,
     },
   ];
 
@@ -20,7 +22,7 @@ const MyPageAlarmSetting = () => {
 
       <section>
         {toggleList.map((item) => (
-          <MyPageAlarmToggle key={item.id} label={item.label} caption={item.caption} apiUrl={item.apiUrl} />
+          <MyPageAlarmToggle key={item.id} label={item.label} caption={item.caption} value={item.value} />
         ))}
       </section>
     </MainLayout>
