a24b5db50f4436c4233e81b758a9576e9d7e77f7
hyoni0817 <hyoni0817@naver.com>
2025-05-30 20:38:22 +0900

    Feat: 공통 체크박스 컴포넌트에 원형 스타일 추가
    - 체크박스에 'circle' variant를 추가하여 원형 디자인을 적용함.
    
    - 원형 체크박스의 체크 표시 위치를 조정하고 관련 CSS 스타일을 추가함.

--- src/components/ui/Checkbox/index.tsx
+++ src/components/ui/Checkbox/index.tsx
@@ -3,14 +3,18 @@ import styles from './styles/styles.module.css';
 
 interface CHeckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
   children: React.ReactElement | React.ReactElement[] | string;
+  variant?: 'default' | 'circle';
 }
 
 const CHeckbox: FC<CHeckboxProps> = (props) => {
-  const { id, children, ...anotherProps } = props;
+  const { id, children, variant = 'default', ...anotherProps } = props;
+
+  const labelClass =
+    variant === 'circle' ? `${styles['common-checkbox-label']} ${styles['circle']}` : styles['common-checkbox-label'];
 
   return (
     <>
-      <label className={styles['common-checkbox-label']} htmlFor={id}>
+      <label className={labelClass} htmlFor={id}>
         <input className={styles['common-checkbox-input']} type="checkbox" id={id} {...anotherProps} />
         {children}
       </label>
