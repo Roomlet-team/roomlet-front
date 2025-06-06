682c081f3618420c3fbfee669bd557ef46ce6887
hyoni0817 <hyoni0817@naver.com>
2025-06-07 01:25:13 +0900

    Feat: 이용 약관 관련 상수 및 타입 정의 추가
    - TERMS 상수를 정의하고, 서비스 이용 약관 및 개인정보 동의 링크를 포함함.
    
    - TermsUrls 인터페이스를 새로 추가하여 약관 URL의 타입을 정의함.

---
+++ src/constant/terms.ts
@@ -0,0 +1,6 @@
+import { TermsUrls } from '../types/terms';
+
+export const TERMS: TermsUrls = {
+  SERVICE_AGREE: 'https://www.notion.so/204bdfc5b2a780de83cae0ddc55c101d?source=copy_link',
+  PRIVACY_AGREE: 'https://www.notion.so/204bdfc5b2a78038b177d3d6a3efa6f8?source=copy_link',
+};
