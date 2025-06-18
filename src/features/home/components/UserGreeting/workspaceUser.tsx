ff6e72531f4515f716acd8d4a954bfe6675b67ea
hyoni0817 <hyoni0817@naver.com>
2025-06-18 12:29:56 +0900

    Feat: 사용자 환영 컴포넌트에 알림 버튼 추가 및 스타일 개선

--- src/features/home/components/UserGreeting/workspaceUser.tsx
+++ src/features/home/components/UserGreeting/workspaceUser.tsx
@@ -2,20 +2,30 @@ import ProfileImg from '@src/components/ui/ProfileImg';
 import stylex from '@stylexjs/stylex';
 import React, { FC } from 'react';
 import useGetWorkspaceMainInfoQuery from '../../queries/useGetWorkspaceMainInfoQuery';
+import Link from 'next/link';
+import AlarmOutlined from '@src/components/icons/AlarmOutlined';
 
 /**
- * 홈 화면에서 워크스페이스에 가입된 사용자에게 인사를 하는 내용을 담은 컴포넌트
+ * 홈 화면에서 워크스페이스에 가입된 사용자에게 환영하는 내용을 담은 컴포넌트
  */
 const WorkspaceUserGreeting = () => {
   const { data } = useGetWorkspaceMainInfoQuery();
 
   return (
     <div {...stylex.props(Styles.container)}>
-      <ProfileImg src={data?.memberInfo.profileImgKey} size={50} borderProperties={{ radius: '50%' }} />
-      <div {...stylex.props(Styles.textContent)}>
-        <p {...stylex.props(Styles.greetingText)}>룸렛에 오신걸 환영해요 🖐🏻</p>
-        <p {...stylex.props(Styles.nicknameText)}>{data?.memberInfo.displayName}님</p>
+      {/* 유저 정보 */}
+      <div {...stylex.props(Styles.userInfoContainer)}>
+        <ProfileImg src={data?.memberInfo.profileImgKey} size={50} borderProperties={{ radius: '50%' }} />
+        <div {...stylex.props(Styles.textContent)}>
+          <p {...stylex.props(Styles.greetingText)}>룸렛에 오신걸 환영해요 🖐🏻</p>
+          <p {...stylex.props(Styles.nicknameText)}>{data?.memberInfo.displayName}님</p>
+        </div>
       </div>
+
+      {/* 알림 이동 링크 */}
+      <Link href="/alarm">
+        <AlarmOutlined width={24} height={24} />
+      </Link>
     </div>
   );
 };
@@ -24,6 +34,11 @@ export default WorkspaceUserGreeting;
 
 const Styles = stylex.create({
   container: {
+    display: 'flex',
+    gap: '16px',
+    alignItems: 'center',
+  },
+  userInfoContainer: {
     width: '100%',
     display: 'flex',
     gap: '16px',
