00b8a93f67073edc7d0e8dc9731b96ce9defe108
hyoni0817 <hyoni0817@naver.com>
2025-06-28 00:21:20 +0900

    Feat: 알림 모두 읽음 처리하는 기능 구현

--- src/pages/alarm.tsx
+++ src/pages/alarm.tsx
@@ -2,16 +2,17 @@ import Header from '@src/components/ui/Header';
 import AlarmCategoryList from '@src/features/alarm/components/AlarmCategoryList';
 import AlarmList from '@src/features/alarm/components/AlarmList';
 import { AlarmCategoryProvider } from '@src/features/alarm/contexts/AlarmCategoryContext';
+import usePatchNotificationsQuery from '@src/features/alarm/queries/usePatchNotificationsQuery';
 import MainLayout from '@src/layouts/MainLayout';
 import React, { useState } from 'react';
 
 const Alarm = () => {
-  const [isReadAll, setIsReadAll] = useState<boolean>(false);
+  const { mutate: patchNotifications } = usePatchNotificationsQuery();
 
   const readAllBtnProps = {
     name: '모두 읽기',
     isActive: true,
-    onClick: () => console.log(''),
+    onClick: () => patchNotifications({ isReadAll: true }),
   };
 
   // 컨텍스트로 알림 카테고리 관리하기
