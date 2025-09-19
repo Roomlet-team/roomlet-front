dd288a87475d91f8061d200ecfd4b4f88933225a
hyoni0817 <hyoni0817@naver.com>
2025-09-20 02:27:19 +0900

    Feat: ProfileImg 컴포넌트에 role 속성 추가 및 아이콘 표시 기능 구현
    - 멤버 권한 타입을 다루는 MemberRole 타입을 선언함.
    
    - 멤버 관련 응답 데이터 타입인 MemberInfoItem과 Member에 role을 추가함.
    
    - ProfileImg 컴포넌트에 role 속성을 추가하여 멤버의 권한에 따라 아이콘을 표시할 수 있도록 수정함.
    
    - BookingMemberSelect, TeamToggle, UserProfile 컴포넌트에서 ProfileImg 사용 시 role 속성을 전달하도록 수정함.

--- src/components/ui/ProfileImg/index.tsx
+++ src/components/ui/ProfileImg/index.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react';
 import stylex from '@stylexjs/stylex';
 import { s3ImgUrlConfig } from '@src/config';
+import CircleAdminFilled from '@src/components/icons/CircleAdminFilled';
+import CircleOwnerFilled from '@src/components/icons/CircleOwnerFilled';
+import { MemberRole } from '@src/shared/types/member';
 
 interface ProfileImgProps extends React.ImgHTMLAttributes<Omit<HTMLImageElement, 'src'>> {
   size: number;
@@ -10,6 +13,7 @@ interface ProfileImgProps extends React.ImgHTMLAttributes<Omit<HTMLImageElement,
     color?: string;
     radius?: string;
   };
+  role?: MemberRole;
 }
 
 /**
@@ -17,21 +21,36 @@ interface ProfileImgProps extends React.ImgHTMLAttributes<Omit<HTMLImageElement,
  * @params imgKey 이미지 S3 키
  * @params size 이미지 크기
  * @params borderProperties border를 적용할 경우 border의 스타일
+ * @params role 멤버 권한 (owner : 최고 관리자, admin : 중간 관리자, member : 멤버)
  */
 const ProfileImg: FC<ProfileImgProps> = (props) => {
-  const { imgKey, size, borderProperties } = props;
+  const { imgKey, size, borderProperties, role } = props;
+
+  const roleIcon = {
+    member: null,
+    admin: <CircleAdminFilled width={20} height={20} />,
+    owner: <CircleOwnerFilled width={20} height={20} />,
+  };
 
   return (
-    <img
-      {...stylex.props(Styles.img(size, borderProperties))}
-      src={imgKey ? `${process.env.NEXT_PUBLIC_S3_URL}/${imgKey}` : s3ImgUrlConfig.defaultProfile}
-    />
+    <div {...stylex.props(Styles.imgContainer)}>
+      <img
+        {...stylex.props(Styles.img(size, borderProperties))}
+        src={imgKey ? `${process.env.NEXT_PUBLIC_S3_URL}/${imgKey}` : s3ImgUrlConfig.defaultProfile}
+      />
+      <div {...stylex.props(Styles.roleIconWrapper, borderProperties?.radius ? Styles.offsetPosition : null)}>
+        {roleIcon[role]}
+      </div>
+    </div>
   );
 };
 
 export default ProfileImg;
 
 const Styles = stylex.create({
+  imgContainer: {
+    position: 'relative',
+  },
   img: (size, borderProperties) => ({
     width: `${size}px`,
     height: `${size}px`,
@@ -43,4 +62,17 @@ const Styles = stylex.create({
     borderColor: borderProperties?.color,
     borderStyle: borderProperties?.width ? 'solid' : null,
   }),
+  roleIconWrapper: {
+    position: 'absolute',
+    display: 'flex',
+    bottom: '0',
+    right: '0',
+    flexShrink: 0,
+    lineHeight: 1,
+    height: 'fit-content',
+  },
+  offsetPosition: {
+    bottom: '-2px',
+    right: '-2px',
+  },
 });
