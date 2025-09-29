1a9c8570f908c11aec2c295ebf501fbbca019ff9
hyoni0817 <hyoni0817@naver.com>
2025-09-29 21:02:27 +0900

    Feat: 멤버 정보 페이지에 팀 검색 기능 구현

--- src/pages/mypage/workspace/member.tsx
+++ src/pages/mypage/workspace/member.tsx
@@ -9,11 +9,16 @@ import useRenderModal from '@src/hooks/ui/useRenderModal';
 import MainLayout from '@src/layouts/MainLayout';
 import AddTeamBottomSheet from '@src/features/mypage/workspace/member/components/AddTeamBottomSheet';
 import useGetMypageInfoQuery from '@src/features/mypage/queries/useGetMypageInfoQuery';
+import useInput from '@src/hooks/useInput';
+import SearchOutlinedV2 from '@src/components/icons/SearchOutlinedV2';
+import useDebounce from '@src/hooks/useDebounce';
 
 const Member = () => {
   const { data: myInfoData } = useGetMypageInfoQuery();
-  const { data } = useGetTeamListQuery();
   const { renderModal } = useRenderModal();
+  const [searchKeyword, handleChangeSearchKeyword] = useInput('');
+  const debounceKeyword = useDebounce(searchKeyword);
+  const { data } = useGetTeamListQuery(debounceKeyword);
 
   const totalMemberCount = data?.teamList?.reduce(
     (prevValue, currentValue) => prevValue + currentValue?.memberList?.length,
@@ -27,9 +32,19 @@ const Member = () => {
   return (
     <MainLayout>
       <Header title="멤버" prevUrl="/mypage/workspace" />
-      {/* 검색 */}
-
       <div {...{ ...stylex.props(Styles.Container) }}>
+        {/* 검색 */}
+        <div {...stylex.props(Styles.SearchContainer)}>
+          <SearchOutlinedV2 width={24} height={24} />
+          <input
+            type="text"
+            placeholder="검색어를 입력해주세요"
+            value={searchKeyword}
+            onChange={handleChangeSearchKeyword}
+            {...stylex.props(Typography.SubTextLargeRegular, Styles.SearchInput)}
+          />
+        </div>
+
         {/* 전체 멤버수 */}
         <div {...stylex.props(Styles.TotalCountWrapper, Typography.TextSmallMedium)}>
           전체 멤버 ({totalMemberCount})
@@ -96,5 +111,23 @@ const Styles = stylex.create({
     background: colors.red500,
     color: colors.white500,
     borderRadius: '20px',
+  },
+  SearchContainer: {
+    marginBottom: '20px',
+    padding: '8px 12px',
+    display: 'flex',
+    gap: '10px',
+    alignItems: 'center',
+    border: `1px solid ${colors.gray40}`,
+    borderRadius: '12px',
+  },
+  SearchInput: {
+    width: '100%',
+    padding: 0,
+    border: 'none',
+    outline: 'none',
+    '::placeholder': {
+      color: colors.gray60,
+    },
   },
 });
