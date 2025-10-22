563fde45120275e3ed02b5c5e2a29d8eb862a1f2
hyoni0817 <hyoni0817@naver.com>
2025-10-22 22:24:53 +0900

    Fix: 예약하기에서 날짜 및 장소 변경을 시도 여부와 상관없이 시간이 초기화되는 문제 해결
    - 새로운 장소가 추가되거나, 새로고침을 할 때 시간이 초기화되어 '시간 선택'으로 나타나는 문제가 있었음.
    
    - 로직에서 선택한 날짜 값이나 장소값이 존재하기만 하면 무조건 시간을 초기화하도록 작성한 코드 때문에 문제가 발생했었음.
    
    - 그래서 실제로 변경할 값을 클릭했을 때에만 시간 선택이 초기화되도록 오류를 해결해줌.

--- src/features/booking/slices/booking.ts
+++ src/features/booking/slices/booking.ts
@@ -2,11 +2,13 @@ import { createSlice } from '@reduxjs/toolkit';
 import { MemberInfoItem } from '@src/queries/team/useGetTeamListQuery';
 
 export interface BookingState {
+  isUpdatedBookingDate: boolean;
   selectBookingDate: string;
   selectBookingMemberObj: { [teamName in string]: MemberInfoItem[] };
 }
 
 const initialState: BookingState = {
+  isUpdatedBookingDate: false,
   selectBookingDate: '',
   selectBookingMemberObj: {},
 };
@@ -15,6 +17,12 @@ export const BookingSlice = createSlice({
   name: 'booking',
   initialState,
   reducers: {
+    resetIsUpdatedBookingDate: (state) => {
+      state.isUpdatedBookingDate = false;
+    },
+    setIsUpdatedBookingDate: (state) => {
+      state.isUpdatedBookingDate = true;
+    },
     // 선택한 회의 예약 날짜
     saveSelectBookingDate: (state, action) => {
       state.selectBookingDate = action.payload;
@@ -27,6 +35,7 @@ export const BookingSlice = createSlice({
 });
 
 // Action creators are generated for each case reducer function
-export const { saveSelectBookingDate, saveSelectBookingMemberObj } = BookingSlice.actions;
+export const { saveSelectBookingDate, saveSelectBookingMemberObj, resetIsUpdatedBookingDate, setIsUpdatedBookingDate } =
+  BookingSlice.actions;
 
 export default BookingSlice.reducer;
