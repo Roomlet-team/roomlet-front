0c5e5b6badcb21538b5a4e768817efe9ce7bf5a3
hyoni0817 <hyoni0817@naver.com>
2025-11-13 17:05:21 +0900

    Feat: ToastContent 컴포넌트에 상태에 따른 아이콘 표시 기능 추가
    - Toast에서만 사용되는 디자인인데 기존 MessageContent라는 이름은 Toast와 상관없이 너무 포괄적인 이름이라서 ToastContent로 변경함.
    
    - type이 존재하지 않을 때, success 상태를 디폴트로 사용했기 때문에 type의 디폴트 값을 success로 설정함.
    
    - toast.promise로 인해 Toast 컨텐츠 컴포넌트 형태만 불러서 이용되는 경우가 있어서 ToastContent 컴포넌트가 export가 되게 함.

--- src/components/ui/Toast/index.tsx
+++ src/components/ui/Toast/index.tsx
@@ -4,28 +4,34 @@ import styles from './styles/styles.module.css';
 import { Typography } from '../../../../public/styles/vars.stylex';
 import stylex from '@stylexjs/stylex';
 import CircleCheckmarkFilled from '@src/components/icons/CircleCheckmarkFilled';
+import CircleFailFilled from '@src/components/icons/CircleFailFilled';
+import Spinner from '@src/components/icons/Spinner';
 
 interface ToastProps {
   message: string;
+  type?: 'success' | 'pending' | 'error';
 }
 
-const MessageContent: FC<ToastProps> = (props) => {
-  const { message } = props;
+export const ToastContent: FC<ToastProps> = (props) => {
+  const { message, type } = props;
+  const typeIcon = {
+    success: <CircleCheckmarkFilled width={24} height={24} />,
+    pending: <Spinner width={24} height={24} spin />,
+    error: <CircleFailFilled width={24} height={24} />,
+  };
 
   return (
     <div {...stylex.props(Styles.Message, Typography.TextSmallMedium)}>
-      <div {...stylex.props(Styles.IconWrapper)}>
-        <CircleCheckmarkFilled width={24} height={24} />
-      </div>
+      <div {...stylex.props(Styles.IconWrapper)}>{typeIcon[type]}</div>
       <span>{message}</span>
     </div>
   );
 };
 
 const Toast: FC<ToastProps> = (props) => {
-  const { message } = props;
+  const { message, type = 'success' } = props;
 
-  return toast(<MessageContent message={message} />, {
+  return toast(<ToastContent message={message} type={type} />, {
     position: 'bottom-center',
     autoClose: 1500,
     closeButton: false,
