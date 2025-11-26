de849966dff22f451ff520e13191e054b4111f68
hyoni0817 <hyoni0817@naver.com>
2025-11-26 14:55:39 +0900

    Feat: 다양한 디바이스용 favicon 링크 추가
    - iOS, Android, Windows 등 다양한 플랫폼에서 사용할 수 있는 favicon 링크를 _app.tsx에 추가함.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,6 +21,7 @@ import '../features/booking/assets/custom-react-calendar.css';
 import '../features/calendar/styles/custom-react-calendar.css';
 import { ToastContainer } from 'react-toastify';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 // import '../features/calendar/styles/styles.css';
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -51,6 +52,25 @@ export default function App({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <HydrationBoundary state={pageProps.dehydratedState}>
         <Provider store={store}>
+          <Head>
+            <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png" />
+            <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png" />
+            <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png" />
+            <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png" />
+            <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png" />
+            <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png" />
+            <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png" />
+            <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png" />
+            <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-icon-192x192.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+            <link rel="manifest" href="/favicon/manifest.json" />
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png" />
+            <meta name="theme-color" content="#ffffff" />
+          </Head>
           <ToastContainer />
           <Component {...pageProps} />
         </Provider>
