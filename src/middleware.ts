be7a514ac142d596fc73e8347081f7e0daec02af
hyoni0817 <hyoni0817@naver.com>
2025-11-26 14:53:08 +0900

    Fix: 미들웨어에서 favicon 폴더 전체 제외하도록 수정
    - 브라우저에서 favicon 폴더 경로로 접근했을 때 로그인 여부를 체크해서 파비콘 이미지를 불러오지 못하는 문제를 발생시켜서 수정함.

--- src/middleware.ts
+++ src/middleware.ts
@@ -36,8 +36,8 @@ export const config = {
      * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
+     * - favicon (favicon folder)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon).*)',
   ],
 };
